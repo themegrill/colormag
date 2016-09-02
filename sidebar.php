633aeccd3b0fb596ca63a5fe312c5110beb2d54e
sanjivshah <sanjiv.saha31@gmail.com>
2016-09-02 15:28:33 +0545

    Grammer fix

--- sidebar.php
+++ sidebar.php
@@ -28,7 +28,7 @@
          the_widget( 'WP_Widget_Text',
             array(
                'title'  => __( 'Example Widget', 'colormag' ),
-               'text'   => sprintf( __( 'This is an example widget to show how the %s Sidebar looks by default. You can add custom widgets from the %swidgets screen%s in the admin. If custom widgets is added than this will be replaced by those widgets.', 'colormag' ), $sidebar_display, current_user_can( 'edit_theme_options' ) ? '<a href="' . admin_url( 'widgets.php' ) . '">' : '', current_user_can( 'edit_theme_options' ) ? '</a>' : '' ),
+               'text'   => sprintf( __( 'This is an example widget to show how the %s Sidebar looks by default. You can add custom widgets from the %swidgets screen%s in the admin. If custom widgets are added then this will be replaced by those widgets.', 'colormag' ), $sidebar_display, current_user_can( 'edit_theme_options' ) ? '<a href="' . admin_url( 'widgets.php' ) . '">' : '', current_user_can( 'edit_theme_options' ) ? '</a>' : '' ),
                'filter' => true,
             ),
             array(
