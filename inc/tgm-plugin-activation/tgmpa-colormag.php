b0b1ffbaa2d6e13e97d471438fd343cb20790aca
Bishal Napit <mebishalnapit@gmail.com>
2017-03-03 11:39:23 +0545

    Remove unrequired comments from tgmpa config

--- inc/tgm-plugin-activation/tgmpa-colormag.php
+++ inc/tgm-plugin-activation/tgmpa-colormag.php
@@ -1,50 +1,22 @@
 <?php
 /**
- * This file represents an example of the code that themes would use to register
- * the required plugins.
- *
- * It is expected that theme authors would copy and paste this code into their
- * functions.php file, and amend to suit.
- *
- * @see http://tgmpluginactivation.com/configuration/ for detailed documentation.
- *
- * @package    TGM-Plugin-Activation
- * @subpackage Example
- * @version    2.6.1 for parent theme Colormag for publication on WordPress.org
- * @author     Thomas Griffin, Gary Jones, Juliette Reinders Folmer
- * @copyright  Copyright (c) 2011, Thomas Griffin
- * @license    http://opensource.org/licenses/gpl-2.0.php GPL v2 or later
- * @link       https://github.com/TGMPA/TGM-Plugin-Activation
+ * This file contains the recommended plugin lists to this theme
  */
 
 add_action( 'tgmpa_register', 'colormag_register_required_plugins' );
 
 /**
- * Register the required plugins for this theme.
- *
- * In this example, we register five plugins:
- * - one included with the TGMPA library
- * - two from an external source, one from an arbitrary source, one from a GitHub repository
- * - two from the .org repo, where one demonstrates the use of the `is_callable` argument
- *
- * The variables passed to the `tgmpa()` function should be:
- * - an array of plugin arrays;
- * - optionally a configuration array.
- * If you are not changing anything in the configuration array, you can remove the array and remove the
- * variable from the function call: `tgmpa( $plugins );`.
- * In that case, the TGMPA default settings will be used.
+ * Register the recommended plugins for this theme.
  *
  * This function is hooked into `tgmpa_register`, which is fired on the WP `init` action on priority 10.
  */
 function colormag_register_required_plugins() {
 
-	/*
+	/**
 	 * Array of plugin arrays. Required keys are name and slug.
-	 * If the source is NOT from the .org repo, then source is also required.
 	 */
 	$plugins = array(
 
-		// Include recommended plugin from the WordPress Plugin Repository.
 		// Include ThemeGrill Demo Importer as recommended
 		array(
 			'name'      => 'ThemeGrill Demo Importer',
@@ -54,15 +26,6 @@ function colormag_register_required_plugins() {
 
 	);
 
-	/*
-	 * Array of configuration settings. Amend each line as needed.
-	 *
-	 * TGMPA will start providing localized text strings soon. If you already have translations of our standard
-	 * strings available, please help us make TGMPA even better by giving us access to these translations or by
-	 * sending in a pull-request with .po file(s) with the translations.
-	 *
-	 * Only uncomment the strings in the config array if you want to customize the strings.
-	 */
 	$config = array(
 		'id'           => 'colormag',              // Unique ID for hashing notices for multiple instances of TGMPA.
 		'default_path' => '',                      // Default absolute path to bundled plugins.
