f11730c32fd8adf3e79d9537763f30342f8f282c
Bishal Napit <mebishalnapit@gmail.com>
2017-12-05 16:20:39 +0545

    Create elementor widgets posts meta function

--- inc/elementor/elementor-functions.php
+++ inc/elementor/elementor-functions.php
@@ -13,7 +13,7 @@
  *
  * @return array of category ids and its respective names
  *
- * @since ColorMag 2.2.3
+ * @since ColorMag 1.2.3
  */
 function colormag_elementor_categories() {
 	$categories = get_categories();
@@ -24,3 +24,100 @@ function colormag_elementor_categories() {
 
 	return $output;
 }
+
+if ( ! function_exists( 'colormag_elementor_widgets_meta' ) ) :
+
+	/**
+	 * Display the posts meta for use within Elementor widgets
+	 *
+	 * @since ColorMag 1.2.3
+	 */
+	function colormag_elementor_widgets_meta() {
+		?>
+
+		<div class="tg-module-meta">
+			<?php
+			$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
+			if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
+				$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
+			}
+			$time_string = sprintf( $time_string,
+				esc_attr( get_the_date( 'c' ) ),
+				esc_html( get_the_date() ),
+				esc_attr( get_the_modified_date( 'c' ) ),
+				esc_html( get_the_modified_date() )
+			);
+			?>
+
+			<span class="tg-post-date entry-date">
+				<?php echo '<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'; ?>
+			</span>
+
+			<span class="tg-post-auther-name author vcard">
+				<?php echo '<a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a>'; ?>
+			</span>
+
+			<span class="tg-module-comments">
+				<?php comments_popup_link( '0', '1', '%' ); ?>
+			</span>
+		</div>
+
+		<?php
+	}
+
+endif;
+
+if ( ! function_exists( 'colormag_elementor_styles' ) ) {
+
+	/**
+	 * Loads styles on elementor editor
+	 *
+	 * @since ColorMag 1.2.3
+	 */
+	function colormag_elementor_styles() {
+		wp_enqueue_style( 'colormag-econs', get_template_directory_uri() . '/inc/elementor/assets/css/colormag-econs.css', false, '1.0' );
+	}
+
+	add_action( 'elementor/editor/after_enqueue_styles', 'colormag_elementor_styles' );
+}
+
+
+if ( ! function_exists( 'colormag_elementor_colored_category' ) ) :
+
+	/**
+	 * Display/Returns the category names for Elementor widgets
+	 *
+	 * @since ColorMag 1.2.3
+	 */
+	function colormag_elementor_colored_category( $display = 1 ) {
+		global $post;
+		$categories = get_the_category();
+		$separator  = '&nbsp;';
+		$output     = '';
+
+		if ( $categories ) {
+			$output .= '<div class="tg-post-categories">';
+			foreach ( $categories as $category ) {
+				$color_code = colormag_category_color( get_cat_id( $category->cat_name ) );
+				if ( ! empty( $color_code ) ) {
+					$output .= '<a href="' . get_category_link( $category->term_id ) . '" style="background:' . colormag_category_color( get_cat_id( $category->cat_name ) ) . '" class="tg-post-category" rel="category tag">' . $category->cat_name . '</a>' . $separator;
+				} else {
+					$output .= '<a href="' . get_category_link( $category->term_id ) . '" class="tg-post-category" rel="category tag">' . $category->cat_name . '</a>' . $separator;
+				}
+			}
+			$output .= '</div>';
+
+			if ( $display == 0 ) {
+				$output = trim( $output, $separator );
+			}
+			if ( $display == 1 ) {
+				echo trim( $output, $separator );
+			}
+		}
+
+		if ( $display == 0 ) {
+			return $output;
+		}
+	}
+
+endif;
