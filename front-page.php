5fa4c1641254563fc2013d6f13583ca0a7c85d22
Bishal Napit <mebishalnapit@gmail.com>
2017-12-11 12:45:18 +0545

    Remove the front page widget areas from displaying when the Page Builder Template is used

--- front-page.php
+++ front-page.php
@@ -2,91 +2,104 @@
 /**
  * Template to show the front page.
  *
- * @package ThemeGrill
+ * @package    ThemeGrill
  * @subpackage ColorMag
- * @since ColorMag 1.0
+ * @since      ColorMag 1.0
  */
-get_header(); ?>
+get_header();
 
-   <div class="front-page-top-section clearfix">
-      <div class="widget_slider_area">
-         <?php
-         if( is_active_sidebar( 'colormag_front_page_slider_area' ) ) {
-            if ( !dynamic_sidebar( 'colormag_front_page_slider_area' ) ):
-            endif;
-         }
-         ?>
-      </div>
+// Do not display the front pages sidebar areas when the Page Builder Template is activated
+if ( is_front_page() && ! is_page_template( 'page-templates/page-builder.php' ) ) : ?>
+	<div class="front-page-top-section clearfix">
+		<div class="widget_slider_area">
+			<?php
+			if ( is_active_sidebar( 'colormag_front_page_slider_area' ) ) {
+				if ( ! dynamic_sidebar( 'colormag_front_page_slider_area' ) ):
+				endif;
+			}
+			?>
+		</div>
 
-      <div class="widget_beside_slider">
-         <?php
-         if( is_active_sidebar( 'colormag_front_page_area_beside_slider' ) ) {
-            if ( !dynamic_sidebar( 'colormag_front_page_area_beside_slider' ) ):
-            endif;
-         }
-         ?>
-      </div>
-   </div>
-   <div class="main-content-section clearfix">
-      <div id="primary">
-         <div id="content" class="clearfix">
+		<div class="widget_beside_slider">
+			<?php
+			if ( is_active_sidebar( 'colormag_front_page_area_beside_slider' ) ) {
+				if ( ! dynamic_sidebar( 'colormag_front_page_area_beside_slider' ) ):
+				endif;
+			}
+			?>
+		</div>
+	</div>
+<?php endif; ?>
 
-         <?php
-         if( is_active_sidebar( 'colormag_front_page_content_top_section' ) ) {
-            if ( !dynamic_sidebar( 'colormag_front_page_content_top_section' ) ):
-            endif;
-         }
+	<div class="main-content-section clearfix">
+		<div id="primary">
+			<div id="content" class="clearfix">
 
-         if( is_active_sidebar( 'colormag_front_page_content_middle_left_section' ) || is_active_sidebar( 'colormag_front_page_content_middle_right_section' )) {
-         ?>
-            <div class="tg-one-half">
-               <?php
-               if ( !dynamic_sidebar( 'colormag_front_page_content_middle_left_section' ) ):
-               endif;
-               ?>
-            </div>
-            <div class="tg-one-half tg-one-half-last">
-               <?php
-               if ( !dynamic_sidebar( 'colormag_front_page_content_middle_right_section' ) ):
-               endif;
-               ?>
-            </div>
-         <div class="clearfix"></div>
-         <?php
-         }
-         if( is_active_sidebar( 'colormag_front_page_content_bottom_section' ) ) {
-            if ( !dynamic_sidebar( 'colormag_front_page_content_bottom_section' ) ):
-            endif;
-         }
-         if (get_theme_mod('colormag_hide_blog_front', 0) == 0): ?>
+				<?php
+				// Do not display the front pages sidebar areas when the Page Builder Template is activated
+				if ( is_front_page() && ! is_page_template( 'page-templates/page-builder.php' ) ) :
 
-            <div class="article-container">
-               <?php if ( have_posts() ) : ?>
+					if ( is_active_sidebar( 'colormag_front_page_content_top_section' ) ) {
+						if ( ! dynamic_sidebar( 'colormag_front_page_content_top_section' ) ):
+						endif;
+					}
 
-                  <?php while ( have_posts() ) : the_post(); ?>
+					if ( is_active_sidebar( 'colormag_front_page_content_middle_left_section' ) || is_active_sidebar( 'colormag_front_page_content_middle_right_section' ) ) {
+						?>
+						<div class="tg-one-half">
+							<?php
+							if ( ! dynamic_sidebar( 'colormag_front_page_content_middle_left_section' ) ):
+							endif;
+							?>
+						</div>
 
-                     <?php
-                     if ( is_front_page() && is_home() ) {
-                       get_template_part( 'content', '' );
-                     } elseif ( is_front_page() ) {
-                       get_template_part( 'content', 'page' );
-                     }
-                     ?>
+						<div class="tg-one-half tg-one-half-last">
+							<?php
+							if ( ! dynamic_sidebar( 'colormag_front_page_content_middle_right_section' ) ):
+							endif;
+							?>
+						</div>
 
-                  <?php endwhile; ?>
+						<div class="clearfix"></div>
+						<?php
+					}
 
-                  <?php get_template_part( 'navigation', 'none' ); ?>
+					if ( is_active_sidebar( 'colormag_front_page_content_bottom_section' ) ) {
+						if ( ! dynamic_sidebar( 'colormag_front_page_content_bottom_section' ) ):
+						endif;
+					}
 
-               <?php else : ?>
+				endif; // Do not display the front pages sidebar areas when the Page Builder Template is activated
 
-                  <?php get_template_part( 'no-results', 'none' ); ?>
+				if ( get_theme_mod( 'colormag_hide_blog_front', 0 ) == 0 ): ?>
 
-               <?php endif; ?>
-            </div>
-         <?php endif; ?>
-         </div>
-      </div>
-      <?php colormag_sidebar_select(); ?>
-   </div>
+					<div class="article-container">
+						<?php if ( have_posts() ) : ?>
+
+							<?php while ( have_posts() ) : the_post(); ?>
+
+								<?php
+								if ( is_front_page() && is_home() ) {
+									get_template_part( 'content', '' );
+								} else if ( is_front_page() ) {
+									get_template_part( 'content', 'page' );
+								}
+								?>
+
+							<?php endwhile; ?>
+
+							<?php get_template_part( 'navigation', 'none' ); ?>
+
+						<?php else : ?>
+
+							<?php get_template_part( 'no-results', 'none' ); ?>
+
+						<?php endif; ?>
+					</div>
+				<?php endif; ?>
+			</div>
+		</div>
+		<?php colormag_sidebar_select(); ?>
+	</div>
 
 <?php get_footer(); ?>
