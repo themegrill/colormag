ce7dd54c61d253c9d0183f47d3d74f61f17a12e2
Ashish Shrestha <ashishsthanp@gmail.com>
2018-03-27 09:47:29 +0545

    Fix - rel attribute value on footer link

--- inc/functions.php
+++ inc/functions.php
@@ -572,7 +572,7 @@ if ( ! function_exists( 'colormag_footer_copyright' ) ) :
 
 		$wp_link = '<a href="https://wordpress.org" target="_blank" title="' . esc_attr__( 'WordPress', 'colormag' ) . '"><span>' . __( 'WordPress', 'colormag' ) . '</span></a>';
 
-		$tg_link = '<a href="https://themegrill.com/themes/colormag" target="_blank" title="' . esc_attr__( 'ThemeGrill', 'colormag' ) . '" rel="designer"><span>' . __( 'ThemeGrill', 'colormag' ) . '</span></a>';
+		$tg_link = '<a href="https://themegrill.com/themes/colormag" target="_blank" title="' . esc_attr__( 'ThemeGrill', 'colormag' ) . '" rel="author"><span>' . __( 'ThemeGrill', 'colormag' ) . '</span></a>';
 
 		$default_footer_value = sprintf( __( 'Copyright &copy; %1$s %2$s. All rights reserved.', 'colormag' ), date( 'Y' ), $site_link ) . '<br>' . sprintf( __( 'Theme: %1$s by %2$s.', 'colormag' ), 'ColorMag', $tg_link ) . ' ' . sprintf( __( 'Powered by %s.', 'colormag' ), $wp_link );
 
