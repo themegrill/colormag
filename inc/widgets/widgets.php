415567ee17feb969466d65ff37c38f6b841e7d63
Nitu Shrestha <shresthanitu984@gmail.com>
2018-07-11 11:33:12 +0545

    Refactor WP Query for featured post (style 1)

--- inc/widgets/widgets.php
+++ inc/widgets/widgets.php
@@ -600,20 +600,35 @@ class colormag_featured_posts_widget extends WP_Widget {
 			$post_status = array( 'auto-draft', 'publish' );
 		}
 
-		if ( $type == 'latest' ) {
-			$get_featured_posts = new WP_Query( array(
-				'posts_per_page'      => $number,
-				'post_type'           => 'post',
-				'ignore_sticky_posts' => true,
-				'post_status'         => $post_status,
-			) );
-		} else {
-			$get_featured_posts = new WP_Query( array(
-				'posts_per_page' => $number,
-				'post_type'      => 'post',
-				'category__in'   => $category,
-			) );
+		$args = array(
+			'posts_per_page'      => $number,
+			'post_type'           => 'post',
+			'ignore_sticky_posts' => true,
+			'no_found_rows'       => true,
+			'post_status'         => $post_status,
+		);
+
+		// Display from category chosen.
+		if ( $type == 'category' ) {
+			$args['category__in'] = $category;
 		}
+
+		$get_featured_posts = new WP_Query( $args );
+
+//		if ( $type == 'latest' ) {
+//			$get_featured_posts = new WP_Query( array(
+//				'posts_per_page'      => $number,
+//				'post_type'           => 'post',
+//				'ignore_sticky_posts' => true,
+//				'post_status'         => $post_status,
+//			) );
+//		} else {
+//			$get_featured_posts = new WP_Query( array(
+//				'posts_per_page' => $number,
+//				'post_type'      => 'post',
+//				'category__in'   => $category,
+//			) );
+//		}
 
 		echo $before_widget;
 		?>
