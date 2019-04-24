326ba7564d28a2473a8a5f9dae795243e9a83d7f
Bishal Napit <mebishalnapit@gmail.com>
2019-04-24 15:45:18 +0545

    Added - ColorMag theme review notice class file

---
+++ inc/admin/class-colormag-theme-review-notice.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * ColorMag Theme Review Notice Class.
+ *
+ * @author  ThemeGrill
+ * @package ColorMag
+ * @since   1.3.9
+ */
+
+// Exit if directly accessed.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Class to display the theme review notice for this theme after certain period.
+ *
+ * Class ColorMag_Theme_Review_Notice
+ */
+class ColorMag_Theme_Review_Notice {
+
+	/**
+	 * Constructor function to include the required functionality for the class.
+	 *
+	 * ColorMag_Theme_Review_Notice constructor.
+	 */
+	public function __construct() {
+
+
+	}
+
+}
+
+new ColorMag_Theme_Review_Notice();
