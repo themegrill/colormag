b242d5ea4ae4cddc296aaba4be42146ba58444c6
Bishal Napit <mebishalnapit@gmail.com>
2019-04-25 12:59:04 +0545

    Add - Theme review notice link

--- inc/admin/class-colormag-theme-review-notice.php
+++ inc/admin/class-colormag-theme-review-notice.php
@@ -41,6 +41,45 @@ class ColorMag_Theme_Review_Notice {
 			update_option( 'colormag_theme_installed_time', time() );
 		}
 
+		add_action( 'admin_notices', array( $this, 'colormag_theme_review_notice' ), 0 );
+
+	}
+
+	/**
+	 * Display the theme review notice.
+	 */
+	public function colormag_theme_review_notice() {
+
+		/**
+		 * Return from notice display if,
+		 * the theme installed is less than 1 month ago.
+		 */
+		if ( get_option( 'colormag_theme_installed_time' ) > strtotime( '-1 month' ) ) {
+			return;
+		}
+		?>
+
+		<div class="notice updated theme-review-notice" style="position:relative;">
+			<?php
+			printf(
+				/* Translators: %1$s current user display name. */
+				esc_html__(
+					'Howdy %1$s! It seems that you have been using this theme for more than 1 month. We hope you are happy with everything that the theme has to offer. If you can spare a mimute, please help us by leaving a 5-star review on WordPress.org.  By spreading the love, we can continue to develop new amazing features in the future, for free!',
+					'colormag'
+				),
+				wp_get_current_user()->display_name
+			)
+			?>
+
+			<div class="links">
+				<a href="https://wordpress.org/support/theme/colormag/reviews/?filter=5#new-post" class="btn button-primary" target="_blank">
+					<span class="dashicons dashicons-external"></span>
+					<span><?php esc_html_e( 'Sure, you deserve it.', 'colormag' ); ?></span>
+				</a>
+			</div>
+		</div>
+
+		<?php
 	}
 
 }
