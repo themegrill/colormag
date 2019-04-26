d9d6c69fdb9c8b0137f196d7131434ac710036cd
Bishal Napit <mebishalnapit@gmail.com>
2019-04-26 11:15:49 +0545

    Fix - Notice duration display time

--- inc/admin/class-colormag-theme-review-notice.php
+++ inc/admin/class-colormag-theme-review-notice.php
@@ -67,7 +67,7 @@ class ColorMag_Theme_Review_Notice {
 		 * 2. If the user has ignored the message partially for 1 month.
 		 * 3. Dismiss always if clicked on 'I Already Did' button.
 		 */
-		if ( ( get_option( 'colormag_theme_installed_time' ) > strtotime( '-1 min' ) ) || ( $ignored_notice_partially > strtotime( '-1 min' ) ) || ( $ignored_notice ) ) {
+		if ( ( get_option( 'colormag_theme_installed_time' ) > strtotime( '-1 month' ) ) || ( $ignored_notice_partially > strtotime( '-1 month' ) ) || ( $ignored_notice ) ) {
 			return;
 		}
 		?>
