51574f17b135dc342c689f586c3b0144baa9ef11
Bishal Napit <mebishalnapit@gmail.com>
2019-07-17 16:36:30 +0545

    Remove - Unrequired classes

--- inc/admin/class-colormag-admin.php
+++ inc/admin/class-colormag-admin.php
@@ -204,7 +204,7 @@ if ( ! class_exists( 'ColorMag_Admin' ) ) :
 					<?php printf( esc_html__( 'Welcome! Thank you for choosing ColorMag! To fully take advantage of the best our theme can offer please make sure you visit our %swelcome page%s.', 'colormag' ), '<a href="' . esc_url( admin_url( 'themes.php?page=colormag-welcome' ) ) . '">', '</a>' ); ?>
 				</p>
 
-				<span class="plugin-install-notice"><?php esc_html_e( 'Clicking the button below will install and activate the ThemeGrill demo importer plugin.', 'colormag' ); ?></span>
+				<span><?php esc_html_e( 'Clicking the button below will install and activate the ThemeGrill demo importer plugin.', 'colormag' ); ?></span>
 
 				<div class="submit">
 					<a class="btn-get-started button button-primary button-hero" href="#" data-name="" data-slug="" aria-label="<?php esc_html_e( 'Get started with ColorMag', 'colormag' ); ?>"><?php esc_html_e( 'Get started with ColorMag', 'colormag' ); ?></a>
