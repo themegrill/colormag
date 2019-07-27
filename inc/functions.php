6ca64e3eb0982d25185f19633e73a20a1dbd561b
Bishal Napit <mebishalnapit@gmail.com>
2019-07-27 12:23:50 +0545

    Fix - Post publish and updated meta

--- inc/functions.php
+++ inc/functions.php
@@ -278,9 +278,9 @@ if ( ! function_exists( 'colormag_entry_meta' ) ) :
 			?>
 
 			<?php
-			$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time>';
+			$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
 			if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
-				$time_string .= '<time class="updated" datetime="%3$s">%4$s</time>';
+				$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
 			}
 			$time_string = sprintf( $time_string,
 				esc_attr( get_the_date( 'c' ) ),
