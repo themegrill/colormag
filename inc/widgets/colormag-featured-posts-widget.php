f84773fe75560be2d6a157e1cd964e799afff38f
Bishal Napit <mebishalnapit@gmail.com>
2019-07-27 14:19:51 +0545

    Fix - Published and Updated class for posts in featured posts widget

--- inc/widgets/colormag-featured-posts-widget.php
+++ inc/widgets/colormag-featured-posts-widget.php
@@ -162,8 +162,15 @@ class colormag_featured_posts_widget extends WP_Widget {
 					</h3>
 					<div class="below-entry-meta">
 						<?php
-						$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time>';
-						$time_string = sprintf( $time_string, esc_attr( get_the_date( 'c' ) ), esc_html( get_the_date() )
+						$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
+						if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
+							$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
+						}
+						$time_string = sprintf( $time_string,
+							esc_attr( get_the_date( 'c' ) ),
+							esc_html( get_the_date() ),
+							esc_attr( get_the_modified_date( 'c' ) ),
+							esc_html( get_the_modified_date() )
 						);
 						printf( __( '<span class="posted-on"><a href="%1$s" title="%2$s" rel="bookmark"><i class="fa fa-calendar-o"></i> %3$s</a></span>', 'colormag' ), esc_url( get_permalink() ), esc_attr( get_the_time() ), $time_string
 						);
