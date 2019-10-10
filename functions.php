e27d729038a7b612a25faf2c6a24dd9f553dd714
Bishal Napit <mebishalnapit@gmail.com>
2019-10-10 11:24:17 +0545

    Add - Customize option class file

--- functions.php
+++ functions.php
@@ -502,6 +502,7 @@ require_once( COLORMAG_INCLUDES_DIR . '/custom-header.php' );
 require_once( COLORMAG_INCLUDES_DIR . '/functions.php' );
 require_once( COLORMAG_INCLUDES_DIR . '/header-functions.php' );
 require_once( COLORMAG_CUSTOMIZER_DIR . '/customizer.php' );
+require_once( COLORMAG_CUSTOMIZER_DIR . '/class-colormag-customizer.php' );
 
 /** Add the Elementor compatibility file */
 if ( defined( 'ELEMENTOR_VERSION' ) ) {
---
+++ inc/customizer/class-colormag-customizer.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * ColorMag customizer class for theme customize options.
+ *
+ * Class ColorMag_Customizer
+ *
+ * @package    ThemeGrill
+ * @subpackage ColorMag
+ * @since      ColorMag 2.0.0
+ */
+
+/**
+ * ColorMag customizer class
+ *
+ * Class ColorMag_Customizer
+ */
+class ColorMag_Customizer {
+
+	/**
+	 * Customizer setup constructor.
+	 *
+	 * ColorMag_Customizer constructor.
+	 */
+	public function __construct() {
+
+	}
+
+}
+
+return new ColorMag_Customizer();
