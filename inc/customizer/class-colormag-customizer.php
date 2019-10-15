1b72a55583410eeb330364bbb4627a954c809001
Bishal Napit <mebishalnapit@gmail.com>
2019-10-15 12:36:47 +0545

    Refactor function for customize options

--- inc/customizer/class-colormag-customizer.php
+++ inc/customizer/class-colormag-customizer.php
@@ -23,21 +23,33 @@ class ColorMag_Customizer {
 	 */
 	public function __construct() {
 
+		// Include the custom controls for customize options.
+		add_action( 'customize_register', array( $this, 'colormag_customize_custom_controls_includes' ) );
+
 		// Include the required files for Customize option.
-		add_action( 'customize_register', array( $this, 'colormag_customize_files_include' ) );
+		add_action( 'customize_register', array( $this, 'colormag_customize_options_file_include' ) );
 
 		// Include the required customize options.
 		add_action( 'customize_register', array( $this, 'colormag_customize_options' ) );
 
-		// Include the custom controls for customize options.
-		add_action( 'customize_register', array( $this, 'colormag_customize_custom_controls_includes' ) );
+	}
+
+	/**
+	 * Include the required files for extending the custom Customize controls.
+	 *
+	 * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+	 */
+	public function colormag_customize_custom_controls_includes( $wp_customize ) {
+
+		require COLORMAG_CUSTOMIZER_DIR . '/custom-controls/class-colormag-upsell-section.php';
+		require COLORMAG_CUSTOMIZER_DIR . '/custom-controls/class-colormag-image-radio-control.php';
 
 	}
 
 	/**
 	 * Include the required files for Customize option.
 	 */
-	public function colormag_customize_files_include() {
+	public function colormag_customize_options_file_include() {
 
 		// Include the customize base option file.
 		require COLORMAG_CUSTOMIZER_DIR . '/options/class-colormag-customize-base-option.php';
@@ -54,18 +66,6 @@ class ColorMag_Customizer {
 	public function colormag_customize_options( $wp_customize ) {
 
 		return apply_filters( 'colormag_customizer_options', array(), $wp_customize );
-
-	}
-
-	/**
-	 * Include the required files for extending the custom Customize controls.
-	 *
-	 * @param WP_Customize_Manager $wp_customize Theme Customizer object.
-	 */
-	public function colormag_customize_custom_controls_includes( $wp_customize ) {
-
-		require COLORMAG_CUSTOMIZER_DIR . '/custom-controls/class-colormag-upsell-section.php';
-		require COLORMAG_CUSTOMIZER_DIR . '/custom-controls/class-colormag-image-radio-control.php';
 
 	}
 
