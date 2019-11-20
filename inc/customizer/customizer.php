7527a120e438fcc1c11f844826d05a8fdf07d7e4
Bishal Napit <mebishalnapit@gmail.com>
2019-11-20 06:31:58 +0545

    Tweak - Remove unrequired function

--- inc/customizer/customizer.php
+++ inc/customizer/customizer.php
@@ -1,16 +1,1 @@
 <?php
-
-/**
- * ColorMag Theme Customizer
- *
- * @package    ThemeGrill
- * @subpackage ColorMag
- * @since      ColorMag 1.0
- */
-function colormag_customize_register( $wp_customize ) {
-
-
-
-}
-
-add_action( 'customize_register', 'colormag_customize_register' );
