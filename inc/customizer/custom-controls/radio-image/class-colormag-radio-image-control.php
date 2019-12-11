24825806db2437deaedd01dd6d6be1ae769a0da4
Bishal Napit <mebishalnapit@gmail.com>
2019-12-11 16:07:40 +0545

    Tweak - Update render_content function to return nothing

--- inc/customizer/custom-controls/radio-image/class-colormag-radio-image-control.php
+++ inc/customizer/custom-controls/radio-image/class-colormag-radio-image-control.php
@@ -36,8 +36,8 @@ class ColorMag_Radio_Image_Control extends WP_Customize_Control {
 		}
 		$this->json['value'] = $this->value();
 
-		$this->json['link']    = $this->get_link();
-		$this->json['id']      = $this->id;
+		$this->json['link'] = $this->get_link();
+		$this->json['id']   = $this->id;
 
 		foreach ( $this->choices as $key => $value ) {
 			$this->json['choices'][ $key ]        = $value['url'];
@@ -62,7 +62,7 @@ class ColorMag_Radio_Image_Control extends WP_Customize_Control {
 	 * Class variables for this control class are available in the `data` JS object;
 	 * export custom variables by overriding {@see WP_Customize_Control::to_json()}.
 	 *
-	 * @see WP_Customize_Control::print_template()
+	 * @see    WP_Customize_Control::print_template()
 	 *
 	 * @access protected
 	 */
@@ -101,68 +101,10 @@ class ColorMag_Radio_Image_Control extends WP_Customize_Control {
 		<?php
 	}
 
+	/**
+	 * Don't render the control content from PHP, as it's rendered via JS on load.
+	 */
 	public function render_content() {
-
-		if ( empty( $this->choices ) ) {
-			return;
-		}
-
-		$name = '_customize-radio-' . $this->id;
-		?>
-		<style>
-			#colormag-img-container .colormag-radio-img-img {
-				border: 3px solid #DEDEDE;
-				margin: 0 5px 5px 0;
-				cursor: pointer;
-				border-radius: 3px;
-				-moz-border-radius: 3px;
-				-webkit-border-radius: 3px;
-			}
-
-			#colormag-img-container .colormag-radio-img-selected {
-				border: 3px solid #AAA;
-				border-radius: 3px;
-				-moz-border-radius: 3px;
-				-webkit-border-radius: 3px;
-			}
-
-			input[type=checkbox]:before {
-				content: '';
-				margin: -3px 0 0 -4px;
-			}
-		</style>
-		<span class="customize-control-title"><?php echo esc_html( $this->label ); ?></span>
-		<ul class="controls" id='colormag-img-container'>
-			<?php
-			foreach ( $this->choices as $value => $label ) :
-				$class = ( $this->value() == $value ) ? 'colormag-radio-img-selected colormag-radio-img-img' : 'colormag-radio-img-img';
-				?>
-				<li style="display: inline;">
-					<label style="margin-left: 0">
-						<input <?php $this->link(); ?>style='display:none' type="radio" value="<?php echo esc_attr( $value ); ?>" name="<?php echo esc_attr( $name ); ?>" <?php
-						$this->link();
-						checked( $this->value(), $value );
-						?> />
-						<img src='<?php echo esc_html( $label ); ?>' class='<?php echo $class; ?>' />
-					</label>
-				</li>
-			<?php
-			endforeach;
-			?>
-		</ul>
-		<script type="text/javascript">
-
-			jQuery( document ).ready( function ( $ ) {
-				$( '.controls#colormag-img-container li img' ).click( function () {
-					$( '.controls#colormag-img-container li' ).each( function () {
-						$( this ).find( 'img' ).removeClass( 'colormag-radio-img-selected' );
-					} );
-					$( this ).addClass( 'colormag-radio-img-selected' );
-				} );
-			} );
-
-		</script>
-		<?php
 	}
 
 }
