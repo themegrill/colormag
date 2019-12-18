60b7d766f181c0ec58bb25790934d3de64b93adf
Bishal Napit <mebishalnapit@gmail.com>
2019-12-18 16:14:03 +0545

    Tweak - Check change state for editor control

--- inc/customizer/custom-controls/editor/editor.js
+++ inc/customizer/custom-controls/editor/editor.js
@@ -22,6 +22,32 @@ wp.customize.controlConstructor[ 'colormag-editor' ] = wp.customize.Control.exte
 			mediaButtons : true
 		} );
 
+	},
+
+	onChangeActive : function ( active, args ) {
+
+		'use strict';
+
+		var control = this,
+		    id      = 'editor_' + control.id,
+		    element = control.container.find( 'textarea' ),
+		    editor;
+
+		editor = tinyMCE.get( id );
+
+		if ( editor ) {
+
+			editor.onChange.add( function ( ed ) {
+				var content;
+
+				ed.save();
+				content = editor.getContent();
+				element.val( content ).trigger( 'change' );
+				wp.customize.instance( control.id ).set( content );
+			} );
+
+		}
+
 	}
 
 } );
