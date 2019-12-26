b14f5689fd77208ad17c762ef3e8b3ff6bd55f9f
Bishal Napit <mebishalnapit@gmail.com>
2019-12-26 11:37:44 +0545

    Add - JS and SCSS file for toggle control

---
+++ inc/customizer/custom-controls/toggle/toggle.js
@@ -0,0 +1,18 @@
+/**
+ * Switch toggle control JS to handle the toggle of custom customize controls.
+ *
+ * File `toggle.js`.
+ *
+ * @package ColorMag
+ */
+wp.customize.controlConstructor[ 'colormag-toggle' ] = wp.customize.Control.extend( {
+
+	ready : function () {
+
+		'use strict';
+
+		var control = this;
+
+	}
+
+} );
