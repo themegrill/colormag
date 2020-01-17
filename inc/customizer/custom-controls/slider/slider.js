21e651a38eb833ed533df7ba2013f3e7e1701116
Bishal Napit <mebishalnapit@gmail.com>
2020-01-17 11:53:09 +0545

    Tweak - Add slider.js customize control file

---
+++ inc/customizer/custom-controls/slider/slider.js
@@ -0,0 +1,18 @@
+/**
+ * Slider control JS to handle the range of the inputs.
+ *
+ * File `slider.js`.
+ *
+ * @package ColorMag
+ */
+wp.customize.controlConstructor[ 'colormag-slider' ] = wp.customize.Control.extend( {
+
+	ready : function () {
+
+		'use strict';
+
+		var control = this;
+
+	}
+
+} );
