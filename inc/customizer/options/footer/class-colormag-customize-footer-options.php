b488beb640528f18f1809673f69f63e86ab44d85
Bishal Napit <mebishalnapit@gmail.com>
2020-02-12 10:37:54 +0545

    Tweak - Move footer main area display type to customize config

--- inc/customizer/options/footer/class-colormag-customize-footer-options.php
+++ inc/customizer/options/footer/class-colormag-customize-footer-options.php
@@ -26,34 +26,28 @@ class ColorMag_Customize_Footer_Options extends ColorMag_Customize_Base_Option {
 	 */
 	public function customizer_options( $options, $wp_customize ) {
 
-		$configs = array();
+		$configs = array(
+
+			// Main total footer area display type option.
+			array(
+				'name'      => 'colormag_main_footer_layout_display_type',
+				'default'   => 'type_one',
+				'type'      => 'control',
+				'control'   => 'radio',
+				'label'     => esc_html__( 'Choose the main total footer area display type that you want', 'colormag' ),
+				'section'   => 'colormag_main_footer_layout_display_type_section',
+				'choices'   => array(
+					'type_one' => esc_html__( 'Type 1 (Default)', 'colormag' ),
+					'type_two' => esc_html__( 'Type 2', 'colormag' ),
+				),
+				'transport' => 'postMessage',
+			),
+
+		);
 
 		$options = array_merge( $options, $configs );
 
 		return $options;
-
-		// Transport postMessage variable set
-		$customizer_selective_refresh = isset( $wp_customize->selective_refresh ) ? 'postMessage' : 'refresh';
-
-		$wp_customize->add_setting( 'colormag_main_footer_layout_display_type', array(
-			'default'           => 'type_one',
-			'capability'        => 'edit_theme_options',
-			'sanitize_callback' => array(
-				'ColorMag_Customizer_Sanitizes',
-				'sanitize_radio_select',
-			),
-			'transport'         => 'postMessage',
-		) );
-
-		$wp_customize->add_control( 'colormag_main_footer_layout_display_type', array(
-			'type'    => 'radio',
-			'label'   => esc_html__( 'Choose the main total footer area display type that you want', 'colormag' ),
-			'section' => 'colormag_main_footer_layout_display_type_section',
-			'choices' => array(
-				'type_one' => esc_html__( 'Type 1 (Default)', 'colormag' ),
-				'type_two' => esc_html__( 'Type 2', 'colormag' ),
-			),
-		) );
 
 	}
 
