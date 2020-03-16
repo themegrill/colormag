9a1f7a3adfc4cb9cceecf51e2873b743c17d3a02
Bishal Napit <mebishalnapit@gmail.com>
2020-03-16 09:50:13 +0545

    Add - Background image control JS file

--- inc/customizer/custom-controls/assets/js/customize-controls.js
+++ inc/customizer/custom-controls/assets/js/customize-controls.js
@@ -1,4 +1,23 @@
 /**
+ * Background image control JS to handle the background customize option.
+ *
+ * File `background.js`.
+ *
+ * @package ColorMag
+ */
+wp.customize.controlConstructor['colormag-background'] = wp.customize.Control.extend( {
+
+	ready : function () {
+
+		'use strict';
+
+		var control = this;
+
+	}
+
+} );
+
+/**
  * Radio buttonset control JS to handle the toggle of radio buttonsets.
  *
  * File `buttonset.js`.
---
+++ inc/customizer/custom-controls/background/background.js
@@ -0,0 +1,18 @@
+/**
+ * Background image control JS to handle the background customize option.
+ *
+ * File `background.js`.
+ *
+ * @package ColorMag
+ */
+wp.customize.controlConstructor['colormag-background'] = wp.customize.Control.extend( {
+
+	ready : function () {
+
+		'use strict';
+
+		var control = this;
+
+	}
+
+} );
