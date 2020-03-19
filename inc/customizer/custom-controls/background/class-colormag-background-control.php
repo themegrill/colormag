007edae7d61657fd8d4f7e655ad3e76a37d89abd
Bishal Napit <mebishalnapit@gmail.com>
2020-03-19 11:23:47 +0545

    Tweak - Add background hidden values via JSON for background customize control

--- inc/customizer/custom-controls/background/class-colormag-background-control.php
+++ inc/customizer/custom-controls/background/class-colormag-background-control.php
@@ -86,111 +86,117 @@ class ColorMag_Background_Control extends WP_Customize_Control {
 				/>
 			</div>
 
-		</div>
+			<div class="background-image">
+				<span class="customize-control-title"><?php esc_html_e( 'Background Image', 'colormag' ); ?></span>
+				<div class="attachment-media-view background-image-upload">
+					<# if ( data.value['background-image'] ) { #>
+					<div class="thumbnail thumbnail-image"><img src="{{ data.value['background-image'] }}" alt="" />
+					</div>
+					<# } else { #>
+					<div class="placeholder"><?php esc_attr_e( 'No Image Selected', 'colormag' ); ?></div>
+					<# } #>
 
-		<div class="background-image">
-			<span class="customize-control-title"><?php esc_html_e( 'Background Image', 'colormag' ); ?></span>
-			<div class="attachment-media-view background-image-upload">
-				<# if ( data.value['background-image'] ) { #>
-				<div class="thumbnail thumbnail-image"><img src="{{ data.value['background-image'] }}" alt="" />
-				</div>
-				<# } else { #>
-				<div class="placeholder"><?php esc_attr_e( 'No Image Selected', 'colormag' ); ?></div>
-				<# } #>
+					<div class="actions">
+						<button data-name="{{ data.name }}"
+						        class="button background-image-upload-remove-button<# if ( ! data.value['background-image'] ) { #> hidden <# } #>"
+						>
+							<?php esc_attr_e( 'Remove', 'colormag' ); ?>
+						</button>
 
-				<div class="actions">
-					<button data-name="{{ data.name }}"
-					        class="button background-image-upload-remove-button<# if ( ! data.value['background-image'] ) { #> hidden <# } #>"
-					>
-						<?php esc_attr_e( 'Remove', 'colormag' ); ?>
-					</button>
-
-					<button data-name="{{ data.name }}"
-					        type="button"
-					        class="button background-image-upload-button"
-					>
-						<?php esc_attr_e( 'Select Image', 'colormag' ); ?>
-					</button>
+						<button data-name="{{ data.name }}"
+						        type="button"
+						        class="button background-image-upload-button"
+						>
+							<?php esc_attr_e( 'Select Image', 'colormag' ); ?>
+						</button>
+					</div>
 				</div>
 			</div>
-		</div>
 
-		<div class="background-repeat">
-			<span class="customize-control-title"><?php esc_html_e( 'Background Repeat', 'colormag' ); ?></span>
-			<select data-name="{{ data.name }}" {{{ data.inputAttrs }}}>
-				<option value="no-repeat"
-				<# if ( 'no-repeat' === data.value['background-repeat'] ) { #> selected <# }
-				#>><?php esc_attr_e( 'No Repeat', 'colormag' ); ?></option>
-				<option value="repeat"
-				<# if ( 'repeat' === data.value['background-repeat'] ) { #> selected <# }
-				#>><?php esc_attr_e( 'Repeat All', 'colormag' ); ?></option>
-				<option value="repeat-x"
-				<# if ( 'repeat-x' === data.value['background-repeat'] ) { #> selected <# }
-				#>><?php esc_attr_e( 'Repeat Horizontally', 'colormag' ); ?></option>
-				<option value="repeat-y"
-				<# if ( 'repeat-y' === data.value['background-repeat'] ) { #> selected <# }
-				#>><?php esc_attr_e( 'Repeat Vertically', 'colormag' ); ?></option>
-			</select>
-		</div>
+			<div class="background-repeat">
+				<span class="customize-control-title"><?php esc_html_e( 'Background Repeat', 'colormag' ); ?></span>
+				<select data-name="{{ data.name }}" {{{ data.inputAttrs }}}>
+					<option value="no-repeat"
+					<# if ( 'no-repeat' === data.value['background-repeat'] ) { #> selected <# }
+					#>><?php esc_attr_e( 'No Repeat', 'colormag' ); ?></option>
+					<option value="repeat"
+					<# if ( 'repeat' === data.value['background-repeat'] ) { #> selected <# }
+					#>><?php esc_attr_e( 'Repeat All', 'colormag' ); ?></option>
+					<option value="repeat-x"
+					<# if ( 'repeat-x' === data.value['background-repeat'] ) { #> selected <# }
+					#>><?php esc_attr_e( 'Repeat Horizontally', 'colormag' ); ?></option>
+					<option value="repeat-y"
+					<# if ( 'repeat-y' === data.value['background-repeat'] ) { #> selected <# }
+					#>><?php esc_attr_e( 'Repeat Vertically', 'colormag' ); ?></option>
+				</select>
+			</div>
 
-		<div class="background-position">
-			<span class="customize-control-title"><?php esc_html_e( 'Background Position', 'colormag' ); ?></span>
-			<select data-name="{{ data.name }}" {{{ data.inputAttrs }}}>
-				<option value="left top"
-				<# if ( 'left top' === data.value['background-position'] ) { #> selected <# }
-				#>><?php esc_attr_e( 'Left Top', 'colormag' ); ?></option>
-				<option value="left center"
-				<# if ( 'left center' === data.value['background-position'] ) { #> selected <# }
-				#>><?php esc_attr_e( 'Left Center', 'colormag' ); ?></option>
-				<option value="left bottom"
-				<# if ( 'left bottom' === data.value['background-position'] ) { #> selected <# }
-				#>><?php esc_attr_e( 'Left Bottom', 'colormag' ); ?></option>
-				<option value="right top"
-				<# if ( 'right top' === data.value['background-position'] ) { #> selected <# }
-				#>><?php esc_attr_e( 'Right Top', 'colormag' ); ?></option>
-				<option value="right center"
-				<# if ( 'right center' === data.value['background-position'] ) { #> selected <# }
-				#>><?php esc_attr_e( 'Right Center', 'colormag' ); ?></option>
-				<option value="right bottom"
-				<# if ( 'right bottom' === data.value['background-position'] ) { #> selected <# }
-				#>><?php esc_attr_e( 'Right Bottom', 'colormag' ); ?></option>
-				<option value="center top"
-				<# if ( 'center top' === data.value['background-position'] ) { #> selected <# }
-				#>><?php esc_attr_e( 'Center Top', 'colormag' ); ?></option>
-				<option value="center center"
-				<# if ( 'center center' === data.value['background-position'] ) { #> selected <# }
-				#>><?php esc_attr_e( 'Center Center', 'colormag' ); ?></option>
-				<option value="center bottom"
-				<# if ( 'center bottom' === data.value['background-position'] ) { #> selected <# }
-				#>><?php esc_attr_e( 'Center Bottom', 'colormag' ); ?></option>
-			</select>
-		</div>
+			<div class="background-position">
+				<span class="customize-control-title"><?php esc_html_e( 'Background Position', 'colormag' ); ?></span>
+				<select data-name="{{ data.name }}" {{{ data.inputAttrs }}}>
+					<option value="left top"
+					<# if ( 'left top' === data.value['background-position'] ) { #> selected <# }
+					#>><?php esc_attr_e( 'Left Top', 'colormag' ); ?></option>
+					<option value="left center"
+					<# if ( 'left center' === data.value['background-position'] ) { #> selected <# }
+					#>><?php esc_attr_e( 'Left Center', 'colormag' ); ?></option>
+					<option value="left bottom"
+					<# if ( 'left bottom' === data.value['background-position'] ) { #> selected <# }
+					#>><?php esc_attr_e( 'Left Bottom', 'colormag' ); ?></option>
+					<option value="right top"
+					<# if ( 'right top' === data.value['background-position'] ) { #> selected <# }
+					#>><?php esc_attr_e( 'Right Top', 'colormag' ); ?></option>
+					<option value="right center"
+					<# if ( 'right center' === data.value['background-position'] ) { #> selected <# }
+					#>><?php esc_attr_e( 'Right Center', 'colormag' ); ?></option>
+					<option value="right bottom"
+					<# if ( 'right bottom' === data.value['background-position'] ) { #> selected <# }
+					#>><?php esc_attr_e( 'Right Bottom', 'colormag' ); ?></option>
+					<option value="center top"
+					<# if ( 'center top' === data.value['background-position'] ) { #> selected <# }
+					#>><?php esc_attr_e( 'Center Top', 'colormag' ); ?></option>
+					<option value="center center"
+					<# if ( 'center center' === data.value['background-position'] ) { #> selected <# }
+					#>><?php esc_attr_e( 'Center Center', 'colormag' ); ?></option>
+					<option value="center bottom"
+					<# if ( 'center bottom' === data.value['background-position'] ) { #> selected <# }
+					#>><?php esc_attr_e( 'Center Bottom', 'colormag' ); ?></option>
+				</select>
+			</div>
 
-		<div class="background-size">
-			<span class="customize-control-title"><?php esc_html_e( 'Background Size', 'colormag' ); ?></span>
-			<select data-name="{{ data.name }}" {{{ data.inputAttrs }}}>
-				<option value="cover"
-				<# if ( 'cover' === data.value['background-size'] ) { #> selected <# }
-				#>><?php esc_attr_e( 'Cover', 'colormag' ); ?></option>
-				<option value="contain"
-				<# if ( 'contain' === data.value['background-size'] ) { #> selected <# }
-				#>><?php esc_attr_e( 'Contain', 'colormag' ); ?></option>
-				<option value="auto"
-				<# if ( 'auto' === data.value['background-size'] ) { #> selected <# }
-				#>><?php esc_attr_e( 'Auto', 'colormag' ); ?></option>
-			</select>
-		</div>
+			<div class="background-size">
+				<span class="customize-control-title"><?php esc_html_e( 'Background Size', 'colormag' ); ?></span>
+				<select data-name="{{ data.name }}" {{{ data.inputAttrs }}}>
+					<option value="cover"
+					<# if ( 'cover' === data.value['background-size'] ) { #> selected <# }
+					#>><?php esc_attr_e( 'Cover', 'colormag' ); ?></option>
+					<option value="contain"
+					<# if ( 'contain' === data.value['background-size'] ) { #> selected <# }
+					#>><?php esc_attr_e( 'Contain', 'colormag' ); ?></option>
+					<option value="auto"
+					<# if ( 'auto' === data.value['background-size'] ) { #> selected <# }
+					#>><?php esc_attr_e( 'Auto', 'colormag' ); ?></option>
+				</select>
+			</div>
 
-		<div class="background-attachment">
-			<span class="customize-control-title"><?php esc_html_e( 'Background Attachment', 'colormag' ); ?></span>
-			<select data-name="{{ data.name }}" {{{ data.inputAttrs }}}>
-				<option value="scroll"
-				<# if ( 'scroll' === data.value['background-attachment'] ) { #> selected <# }
-				#>><?php esc_attr_e( 'Scroll', 'colormag' ); ?></option>
-				<option value="fixed"
-				<# if ( 'fixed' === data.value['background-attachment'] ) { #> selected <# }
-				#>><?php esc_attr_e( 'Fixed', 'colormag' ); ?></option>
-			</select>
+			<div class="background-attachment">
+				<span class="customize-control-title"><?php esc_html_e( 'Background Attachment', 'colormag' ); ?></span>
+				<select data-name="{{ data.name }}" {{{ data.inputAttrs }}}>
+					<option value="scroll"
+					<# if ( 'scroll' === data.value['background-attachment'] ) { #> selected <# }
+					#>><?php esc_attr_e( 'Scroll', 'colormag' ); ?></option>
+					<option value="fixed"
+					<# if ( 'fixed' === data.value['background-attachment'] ) { #> selected <# }
+					#>><?php esc_attr_e( 'Fixed', 'colormag' ); ?></option>
+				</select>
+			</div>
+
+			<input class="background-hidden-value"
+			       value="{{ JSON.stringify( data.value ) }}"
+			       data-name="{{data.name}}"
+			       type="hidden" {{{ data.link }}}
+			>
+
 		</div>
 
 		<?php
