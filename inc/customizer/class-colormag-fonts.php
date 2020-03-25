f8fd851ab336a8e730dfdb3ce9d2cc7a686b5eeb
Bishal Napit <mebishalnapit@gmail.com>
2020-03-25 07:29:38 +0545

    Tweak - Get system fonts as an array

--- inc/customizer/class-colormag-fonts.php
+++ inc/customizer/class-colormag-fonts.php
@@ -19,5 +19,47 @@ if ( ! defined( 'ABSPATH' ) ) {
  */
 class ColorMag_Fonts {
 
+	/**
+	 * System Fonts
+	 *
+	 * @var array
+	 */
+	public static $system_fonts = array();
+
+	/**
+	 * Google Fonts
+	 *
+	 * @var array
+	 */
+	public static $google_fonts = array();
+
+	/**
+	 * Get system fonts.
+	 *
+	 * @return mixed|void
+	 */
+	public static function get_system_fonts() {
+
+		if ( empty( self::$system_fonts ) ) :
+
+			self::$system_fonts = array(
+
+				'serif'      => array(
+					'fallback' => 'Georgia,Times,"Times New Roman",serif',
+				),
+				'sans-serif' => array(
+					'fallback' => '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen-Sans, Ubuntu, Cantarell, "Helvetica Neue", Helvetica, Arial, sans-serif',
+				),
+				'monospace'  => array(
+					'fallback' => 'Monaco,"Lucida Sans Typewriter","Lucida Typewriter","Courier New",Courier,monospace',
+				),
+
+			);
+
+		endif;
+
+		return apply_filters( 'colormag_system_fonts', self::$system_fonts );
+
+	}
 
 }
