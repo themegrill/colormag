3237a8ccb2ac98d09c9c0e013ac5b1264aeb4bc8
Bishal Napit <mebishalnapit@gmail.com>
2020-03-26 11:06:03 +0545

    Tweak - Enqueue customize control via ColorMag_Customize_Base_Additional_Control class

--- inc/customizer/custom-controls/class-colormag-customize-base-additional-control.php
+++ inc/customizer/custom-controls/class-colormag-customize-base-additional-control.php
@@ -24,6 +24,61 @@ class ColorMag_Customize_Base_Additional_Control extends WP_Customize_Control {
 	 */
 	public function enqueue() {
 
+		$suffix = ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) ? '' : '.min';
+
+		/**
+		 * Enqueue required Customize Controls CSS files.
+		 */
+		// SelectWoo CSS library file.
+		wp_enqueue_style(
+			'selectWoo',
+			COLORMAG_CUSTOMIZER_URL . '/custom-controls/assets/css/selectWoo' . $suffix . '.css',
+			array(),
+			COLORMAG_THEME_VERSION
+		);
+
+		// Main CSS file.
+		wp_enqueue_style(
+			'colormag-customize-controls',
+			COLORMAG_CUSTOMIZER_URL . '/custom-controls/assets/css/customize-controls' . $suffix . '.css',
+			array(),
+			COLORMAG_THEME_VERSION
+		);
+
+		/**
+		 * Enqueue required Customize Controls JS files.
+		 */
+		// SelectWoo JS library file.
+		wp_enqueue_script(
+			'selectWoo',
+			COLORMAG_CUSTOMIZER_URL . '/custom-controls/assets/js/selectWoo' . $suffix . '.js',
+			array(),
+			COLORMAG_THEME_VERSION,
+			true
+		);
+
+		// WP Color Picker Alpha JS library file.
+		wp_enqueue_script(
+			'wp-color-picker-alpha',
+			COLORMAG_CUSTOMIZER_URL . '/custom-controls/assets/js/wp-color-picker-alpha' . $suffix . '.js',
+			array(
+				'wp-color-picker',
+			),
+			COLORMAG_THEME_VERSION,
+			true
+		);
+
+		// Main JS file.
+		wp_enqueue_script(
+			'colormag-customize-controls',
+			COLORMAG_CUSTOMIZER_URL . '/custom-controls/assets/js/customize-controls' . $suffix . '.js',
+			array(
+				'jquery',
+			),
+			COLORMAG_THEME_VERSION,
+			true
+		);
+
 	}
 
 }
