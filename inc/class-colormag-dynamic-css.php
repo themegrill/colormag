989f00ad5eb337b0be0b9ff8997dcaa6dd288744
Bishal Napit <mebishalnapit@gmail.com>
2020-04-06 11:02:56 +0545

    Tweak - Initialize dynamic CSS generation file

---
+++ inc/class-colormag-dynamic-css.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * ColorMag dynamic CSS generation file for theme options.
+ *
+ * Class ColorMag_Dynamic_CSS
+ *
+ * @package    ThemeGrill
+ * @subpackage ColorMag
+ * @since      ColorMag 2.0.0
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * ColorMag dynamic CSS generation file for theme options.
+ *
+ * Class ColorMag_Dynamic_CSS
+ */
+class ColorMag_Dynamic_CSS {
+
+	/**
+	 * Customizer dynamic CSS generation constructor.
+	 *
+	 * ColorMag_Dynamic_CSS constructor.
+	 */
+	public function __construct() {
+
+
+	}
+
+}
+
+return new ColorMag_Dynamic_CSS();
