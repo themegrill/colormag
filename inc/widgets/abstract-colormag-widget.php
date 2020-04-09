e8a9ff25b38c7a1367221d887ed545c34d2b01aa
Bishal Napit <mebishalnapit@gmail.com>
2020-04-09 10:56:18 +0545

    Tweak - Widget abstract class

--- inc/widgets/abstract-colormag-widget.php
+++ inc/widgets/abstract-colormag-widget.php
@@ -21,5 +21,146 @@ if ( ! defined( 'ABSPATH' ) ) {
  */
 abstract class ColorMag_Widget extends WP_Widget {
 
+	/**
+	 * CSS class.
+	 *
+	 * @var string
+	 */
+	public $widget_cssclass;
+
+	/**
+	 * Widget description.
+	 *
+	 * @var string
+	 */
+	public $widget_description;
+
+	/**
+	 * Widget ID.
+	 *
+	 * @var string
+	 */
+	public $widget_id;
+
+	/**
+	 * Widget name.
+	 *
+	 * @var string
+	 */
+	public $widget_name;
+
+	/**
+	 * Settings.
+	 *
+	 * @var array
+	 */
+	public $settings = array();
+
+	/**
+	 * Widget Control Options.
+	 *
+	 * @var array
+	 */
+	public $control_options = array();
+
+	/**
+	 * Constructor.
+	 */
+	public function __construct() {
+
+		$widget_options = array(
+			'classname'                   => $this->widget_cssclass,
+			'description'                 => $this->widget_description,
+			'customize_selective_refresh' => true,
+		);
+
+		parent::__construct( $this->widget_id, $this->widget_name, $widget_options, $this->control_options );
+
+	}
+
+	/**
+	 * Updates a particular instance of a widget.
+	 *
+	 * @param array $new_instance New instance.
+	 * @param array $old_instance Old instance.
+	 *
+	 * @return array
+	 * @see    WP_Widget->update
+	 */
+	public function update( $new_instance, $old_instance ) {
+
+		$instance = $old_instance;
+
+		if ( empty( $this->settings ) ) {
+			return $instance;
+		}
+
+		// Loop settings and get values to save.
+		foreach ( $this->settings as $key => $setting ) {
+			if ( ! isset( $setting['type'] ) ) {
+				continue;
+			}
+
+			// Format the value based on settings type.
+			switch ( $setting['type'] ) {
+				default:
+					$instance[ $key ] = isset( $new_instance[ $key ] ) ? sanitize_text_field( $new_instance[ $key ] ) : $setting['default'];
+					break;
+			}
+
+			/**
+			 * Sanitize the value of a setting.
+			 */
+			$instance[ $key ] = apply_filters( 'colormag_widget_settings_sanitize_option', $instance[ $key ], $new_instance, $key, $setting );
+		}
+
+		return $instance;
+
+	}
+
+	/**
+	 * Outputs the settings update form.
+	 *
+	 * @param array $instance Instance.
+	 *
+	 * @see   WP_Widget->form
+	 */
+	public function form( $instance ) {
+
+		if ( empty( $this->settings ) ) {
+			return;
+		}
+
+		foreach ( $this->settings as $key => $setting ) {
+
+			$class = isset( $setting['class'] ) ? $setting['class'] : '';
+			$value = isset( $instance[ $key ] ) ? $instance[ $key ] : $setting['default'];
+
+			switch ( $setting['type'] ) {
+
+				case 'text':
+					?>
+					<p>
+						<label for="<?php echo esc_attr( $this->get_field_id( $key ) ); ?>">
+							<?php echo esc_html( $setting['label'] ); ?>
+						</label>
+						<input type="text"
+						       class="widefat <?php echo esc_attr( $class ); ?>"
+						       id="<?php echo esc_attr( $this->get_field_id( $key ) ); ?>"
+						       name="<?php echo esc_attr( $this->get_field_name( $key ) ); ?>"
+						       value="<?php echo esc_attr( $value ); ?>"
+						/>
+					</p>
+					<?php
+					break;
+
+				// Default: run an action.
+				default:
+					do_action( 'colormag_widget_field_' . $setting['type'], $key, $value, $setting, $instance );
+					break;
+			}
+		}
+
+	}
 
 }
