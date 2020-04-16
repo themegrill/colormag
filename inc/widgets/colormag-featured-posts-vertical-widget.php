70004d66a51808c9dceef3af8f7a3e0c2a9c3e5b
Bishal Napit <mebishalnapit@gmail.com>
2020-04-16 09:10:11 +0545

    Tweak - Modify rendering of featured posts style 2 widget

--- inc/widgets/colormag-featured-posts-vertical-widget.php
+++ inc/widgets/colormag-featured-posts-vertical-widget.php
@@ -79,22 +79,20 @@ class colormag_featured_posts_vertical_widget extends ColorMag_Widget {
 	 * @see WP_Widget
 	 */
 	public function widget( $args, $instance ) {
-		extract( $args );
-		extract( $instance );
 
 		global $post;
-		$title    = isset( $instance['title'] ) ? $instance['title'] : '';
+		$title    = apply_filters( 'widget_title', isset( $instance['title'] ) ? $instance['title'] : '' );
 		$text     = isset( $instance['text'] ) ? $instance['text'] : '';
 		$number   = empty( $instance['number'] ) ? 4 : $instance['number'];
 		$type     = isset( $instance['type'] ) ? $instance['type'] : 'latest';
 		$category = isset( $instance['category'] ) ? $instance['category'] : '';
 
 		$post_status = 'publish';
-		if ( get_option( 'fresh_site' ) == 1 ) {
+		if ( 1 == get_option( 'fresh_site' ) ) {
 			$post_status = array( 'auto-draft', 'publish' );
 		}
 
-		$args = array(
+		$query_args = array(
 			'posts_per_page'      => $number,
 			'post_type'           => 'post',
 			'ignore_sticky_posts' => true,
@@ -103,41 +101,51 @@ class colormag_featured_posts_vertical_widget extends ColorMag_Widget {
 		);
 
 		// Display from category chosen.
-		if ( $type == 'category' ) {
-			$args['category__in'] = $category;
+		if ( 'category' == $type ) {
+			$query_args['category__in'] = $category;
 		}
 
-		$get_featured_posts = new WP_Query( $args );
+		$get_featured_posts = new WP_Query( $query_args );
 
-		echo $before_widget;
+		$this->widget_start( $args );
 		?>
 		<?php
-		if ( $type != 'latest' ) {
+		$border_color = '';
+		$title_color  = '';
+		if ( 'latest' != $type ) {
 			$border_color = 'style="border-bottom-color:' . colormag_category_color( $category ) . ';"';
 			$title_color  = 'style="background-color:' . colormag_category_color( $category ) . ';"';
-		} else {
-			$border_color = '';
-			$title_color  = '';
 		}
+
+		// Display the title.
 		if ( ! empty( $title ) ) {
-			echo '<h3 class="widget-title" ' . $border_color . '><span ' . $title_color . '>' . esc_html( $title ) . '</span></h3>';
+			echo '<h3 class="widget-title" ' . $border_color . '><span ' . $title_color . '>' . esc_html( $title ) . '</span></h3>'; // phpcs:ignore WordPress.XSS.EscapeOutput.OutputNotEscaped
 		}
+
+		// Display the description.
 		if ( ! empty( $text ) ) {
-			?> <p> <?php echo esc_textarea( $text ); ?> </p> <?php } ?>
-		<?php
-		$i = 1;
-		while ( $get_featured_posts->have_posts() ):$get_featured_posts->the_post();
 			?>
-			<?php if ( $i == 1 ) {
+			<p><?php echo wp_kses_post( $text ); ?></p>
+			<?php
+		}
+
+		$i = 1;
+		while ( $get_featured_posts->have_posts() ) :
+			$get_featured_posts->the_post();
+
+			if ( 1 == $i ) {
 				$featured = 'colormag-featured-post-medium';
 			} else {
 				$featured = 'colormag-featured-post-small';
-			} ?>
-			<?php if ( $i == 1 ) {
+			}
+
+			if ( 1 == $i ) {
 				echo '<div class="first-post">';
-			} elseif ( $i == 2 ) {
+			} elseif ( 2 == $i ) {
 				echo '<div class="following-post">';
-			} ?>
+			}
+			?>
+
 			<div class="single-article clearfix">
 				<?php
 				if ( has_post_thumbnail() ) {
@@ -145,45 +153,35 @@ class colormag_featured_posts_vertical_widget extends ColorMag_Widget {
 					$thumbnail_id    = get_post_thumbnail_id( $post->ID );
 					$image_alt_text  = get_post_meta( $thumbnail_id, '_wp_attachment_image_alt', true );
 					$title_attribute = get_the_title( $post->ID );
-					if ( empty( $image_alt_text ) ) {
-						$image_alt_text = $title_attribute;
-					}
-					$image .= '<figure>';
-					$image .= '<a href="' . get_permalink() . '" title="' . the_title( '', '', false ) . '">';
-					$image .= get_the_post_thumbnail( $post->ID, $featured, array(
+					$image_alt_text  = empty( $image_alt_text ) ? $title_attribute : $image_alt_text;
+					$image           .= '<figure>';
+					$image           .= '<a href="' . get_permalink() . '" title="' . the_title_attribute( 'echo=0' ) . '">';
+					$image           .= get_the_post_thumbnail(
+						$post->ID,
+						$featured,
+						array(
 							'title' => esc_attr( $title_attribute ),
 							'alt'   => esc_attr( $image_alt_text ),
-						) ) . '</a>';
-					$image .= '</figure>';
-					echo $image;
+						)
+					);
+					$image           .= '</a>';
+					$image           .= '</figure>';
+
+					echo $image; // phpcs:ignore WordPress.XSS.EscapeOutput.OutputNotEscaped
 				}
 				?>
+
 				<div class="article-content">
 					<?php colormag_colored_category(); ?>
 					<h3 class="entry-title">
-						<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a>
+						<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
+							<?php the_title(); ?>
+						</a>
 					</h3>
-					<div class="below-entry-meta">
-						<?php
-						$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
-						if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
-							$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
-						}
-						$time_string = sprintf( $time_string,
-							esc_attr( get_the_date( 'c' ) ),
-							esc_html( get_the_date() ),
-							esc_attr( get_the_modified_date( 'c' ) ),
-							esc_html( get_the_modified_date() )
-						);
-						printf( __( '<span class="posted-on"><a href="%1$s" title="%2$s" rel="bookmark"><i class="fa fa-calendar-o"></i> %3$s</a></span>', 'colormag' ), esc_url( get_permalink() ), esc_attr( get_the_time() ), $time_string
-						);
-						?>
-						<span class="byline"><span class="author vcard"><i class="fa fa-user"></i><a class="url fn n" href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>" title="<?php echo get_the_author(); ?>"><?php echo esc_html( get_the_author() ); ?></a></span></span>
-						<?php if ( ! post_password_required() && comments_open() ) { ?>
-							<span class="comments"><i class="fa fa-comment"></i><?php comments_popup_link( '0', '1', '%' ); ?></span>
-						<?php } ?>
-					</div>
-					<?php if ( $i == 1 ) { ?>
+
+					<?php $this->entry_meta(); ?>
+
+					<?php if ( 1 == $i ) { ?>
 						<div class="entry-content">
 							<?php the_excerpt(); ?>
 						</div>
@@ -191,20 +189,21 @@ class colormag_featured_posts_vertical_widget extends ColorMag_Widget {
 				</div>
 
 			</div>
-			<?php if ( $i == 1 ) {
-				echo '</div>';
-			} ?>
 			<?php
+			if ( 1 == $i ) {
+				echo '</div>';
+			}
 			$i ++;
 		endwhile;
 		if ( $i > 2 ) {
 			echo '</div>';
 		}
-		// Reset Post Data
-		wp_reset_query();
-		?>
-		<?php
-		echo $after_widget;
+
+		// Reset Post Data.
+		wp_reset_postdata();
+
+		$this->widget_end( $args );
+
 	}
 
 }
