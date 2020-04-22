e15178203f5aa0772307842d64fe46f3023113d3
Bishal Napit <mebishalnapit@gmail.com>
2020-04-22 16:01:26 +0545

    Tweak - Modify render function of grid style 2 widget

--- inc/elementor/widgets/colormag-elementor-widgets-grid-2.php
+++ inc/elementor/widgets/colormag-elementor-widgets-grid-2.php
@@ -83,108 +83,78 @@ class ColorMag_Elementor_Widgets_Grid_2 extends Colormag_Elementor_Widget_Base {
 	 */
 	protected function render() {
 
+		$widget_title        = $this->get_settings( 'widget_title' );
 		$posts_number        = $this->get_settings( 'posts_number' );
 		$display_type        = $this->get_settings( 'display_type' );
 		$offset_posts_number = $this->get_settings( 'offset_posts_number' );
 		$categories_selected = $this->get_settings( 'categories_selected' );
 
-		$args = array(
-			'posts_per_page'      => $posts_number,
-			'post_type'           => 'post',
-			'ignore_sticky_posts' => true,
-			'no_found_rows'       => true,
-		);
-
-		// Display from the category selected
-		if ( 'categories' == $display_type ) {
-			$args[ 'category__in' ] = $categories_selected;
-		}
-
-		// Offset the posts
-		if ( ! empty( $offset_posts_number ) ) {
-			$args[ 'offset' ] = $offset_posts_number;
-		}
-
-		// Start the WP_Query Object/Class
-		$get_featured_posts = new \WP_Query( $args );
+		// Create the posts query.
+		$get_featured_posts = $this->query_posts( $posts_number, $display_type, $categories_selected,$offset_posts_number );
 		?>
 
 		<div class="tg-module-grid tg-module-grid--style-2 tg-module-wrapper">
 			<?php
-			$widget_title = $this->get_settings( 'widget_title' );
-			if ( ! empty( $widget_title ) ) : ?>
-				<div class="tg-module-title-wrap">
-					<h4 class="module-title">
-						<span><?php echo $this->get_settings( 'widget_title' ); ?></span>
-					</h4>
-				</div>
-			<?php endif;
+			// Displays the widget title.
+			$this->widget_title( $widget_title );
 			?>
 
 			<div class="tg-row thinner">
 				<?php
 				$count = 1;
 				while ( $get_featured_posts->have_posts() ) :
-					$get_featured_posts->the_post(); ?>
+					$get_featured_posts->the_post();
 
-					<?php
 					$thumbnail_image_size        = 'colormag-highlighted-post';
 					$main_div_loop_wrapper_class = 'tg_module_grid tg_module_grid--small tg_module_grid--half tg-col-control';
 
-					if ( $count == 1 ) {
-
+					if ( 1 == $count ) {
 						$thumbnail_image_size        = 'colormag-elementor-grid-large-thumbnail';
 						$main_div_loop_wrapper_class = 'tg_module_grid tg_module_grid--full';
-
-					} else if ( $count == 2 ) {
-
+					} elseif ( 2 == $count ) {
 						$thumbnail_image_size        = 'colormag-elementor-grid-medium-large-thumbnail';
 						$main_div_loop_wrapper_class = 'tg_module_grid tg_module_grid--small tg_module_grid--full tg-col-control';
-
 					}
 
-					if ( ( $count == 1 ) || ( $count == 2 ) ) :
+					if ( ( 1 == $count ) || ( 2 == $count ) ) :
 						echo '<div class="tg-col-control">';
 					endif;
-					if ( $count == 2 ) :
+					if ( 2 == $count ) :
 						echo '<div class="tg-row thinner">';
 					endif;
 					?>
 
 					<div class="<?php echo esc_attr( $main_div_loop_wrapper_class ); ?>">
-						<?php
-						if ( has_post_thumbnail() ) : ?>
+						<?php if ( has_post_thumbnail() ) : ?>
 							<figure class="tg-module-thumb">
-								<a href="<?php the_permalink(); ?>" class="tg-thumb-link">
-									<?php the_post_thumbnail( $thumbnail_image_size ); ?>
-								</a>
+								<?php $this->the_post_thumbnail( $thumbnail_image_size ); ?>
 							</figure>
-						<?php endif;
-						?>
+						<?php endif; ?>
 
 						<div class="tg-module-info">
-							<?php colormag_elementor_colored_category(); ?>
+							<?php
+							colormag_elementor_colored_category();
 
-							<h3 class="tg-module-title entry-title">
-								<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-							</h3>
+							// Display the post title.
+							$this->the_title();
+							?>
 						</div>
 					</div>
 
 					<?php
-					if ( $count == 1 ) :
+					if ( 1 == $count ) :
 						echo '</div>';
 					endif;
 
 					$count ++;
 				endwhile;
 
-				if ( $count > 2 ) {
+				if ( 2 < $count ) {
 					echo '</div>';
 					echo '</div>';
 				}
 
-				// Reset the postdata
+				// Reset the postdata.
 				wp_reset_postdata();
 				?>
 			</div>
@@ -192,4 +162,5 @@ class ColorMag_Elementor_Widgets_Grid_2 extends Colormag_Elementor_Widget_Base {
 
 		<?php
 	}
+
 }
