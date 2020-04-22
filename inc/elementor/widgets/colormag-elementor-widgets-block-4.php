958be72befe0bf8512c3520f4f36c65d19233609
Bishal Napit <mebishalnapit@gmail.com>
2020-04-22 13:24:02 +0545

    Tweak - Update render function for widget style 4

--- inc/elementor/widgets/colormag-elementor-widgets-block-4.php
+++ inc/elementor/widgets/colormag-elementor-widgets-block-4.php
@@ -76,67 +76,45 @@ class ColorMag_Elementor_Widgets_Block_4 extends Colormag_Elementor_Widget_Base 
 	 */
 	protected function render() {
 
+		$widget_title        = $this->get_settings( 'widget_title' );
 		$posts_number        = $this->get_settings( 'posts_number' );
 		$display_type        = $this->get_settings( 'display_type' );
 		$offset_posts_number = $this->get_settings( 'offset_posts_number' );
 		$categories_selected = $this->get_settings( 'categories_selected' );
 
-		$args = array(
-			'posts_per_page'      => $posts_number,
-			'post_type'           => 'post',
-			'ignore_sticky_posts' => true,
-			'no_found_rows'       => true,
-		);
-
-		// Display from the category selected
-		if ( 'categories' == $display_type ) {
-			$args[ 'category__in' ] = $categories_selected;
-		}
-
-		// Offset the posts
-		if ( ! empty( $offset_posts_number ) ) {
-			$args[ 'offset' ] = $offset_posts_number;
-		}
-
-		// Start the WP_Query Object/Class
-		$get_featured_posts = new \WP_Query( $args );
+		// Create the posts query.
+		$get_featured_posts = $this->query_posts( $posts_number, $display_type, $categories_selected,$offset_posts_number );
 		?>
 
 		<div class="tg-module-block tg-module-block--style-4 tg-module-wrapper">
 			<?php
-			$widget_title = $this->get_settings( 'widget_title' );
-			if ( ! empty( $widget_title ) ) : ?>
-				<div class="tg-module-title-wrap">
-					<h4 class="module-title">
-						<span><?php echo $this->get_settings( 'widget_title' ); ?></span>
-					</h4>
-				</div>
-			<?php endif;
-			?>
-			<?php
+			// Displays the widget title.
+			$this->widget_title( $widget_title );
+
 			while ( $get_featured_posts->have_posts() ) :
-				$get_featured_posts->the_post(); ?>
+				$get_featured_posts->the_post();
+				?>
+
 				<div class="tg_module_block tg-row">
-					<?php
-					if ( has_post_thumbnail() ) : ?>
+					<?php if ( has_post_thumbnail() ) : ?>
 						<figure class="tg-module-thumb tg-col-control">
-							<a href="<?php the_permalink(); ?>" class="tg-thumb-link">
-								<?php the_post_thumbnail( 'colormag-highlighted-post' ); ?>
-							</a>
+							<?php $this->the_post_thumbnail( 'colormag-highlighted-post' ); ?>
 						</figure>
-					<?php endif;
-					?>
+					<?php endif; ?>
 
 					<div class="tg-module-info tg-col-control">
-						<h3 class="tg-module-title entry-title">
-							<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-						</h3>
+						<?php
+						// Display the post title.
+						$this->the_title();
 
-						<?php colormag_elementor_widgets_meta(); // Displays the entry meta
+						// Displays the entry meta.
+						colormag_elementor_widgets_meta();
 						?>
 
 						<div class="tg-excerpt entry-content">
-							<?php the_excerpt(); // Displays the post excerpts
+							<?php
+							// Displays the post excerpts.
+							the_excerpt();
 							?>
 						</div>
 					</div>
@@ -145,7 +123,7 @@ class ColorMag_Elementor_Widgets_Block_4 extends Colormag_Elementor_Widget_Base 
 				<?php
 			endwhile;
 
-			// Reset the postdata
+			// Reset the postdata.
 			wp_reset_postdata();
 			?>
 		</div>
