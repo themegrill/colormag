3775ddad07e6edd6a5ba37630ff402c189a1d11a
Bishal Napit <mebishalnapit@gmail.com>
2020-04-22 16:51:07 +0545

    Tweak - Update grid style 5 widget settings to utilize ColorMag Elementor Widget Base class

--- inc/elementor/elementor.php
+++ inc/elementor/elementor.php
@@ -152,7 +152,7 @@ if ( ! function_exists( 'colormag_elementor_widgets_registered' ) ) :
 		$widgets_manager->register_widget_type( new ColorMag_Elementor_Widgets_Grid_2() );
 		$widgets_manager->register_widget_type( new ColorMag_Elementor_Widgets_Grid_3() );
 		$widgets_manager->register_widget_type( new ColorMag_Elementor_Widgets_Grid_4() );
-		$widgets_manager->register_widget_type( new \Elementor\ColorMag_Elementor_Widgets_Grid_5() );
+		$widgets_manager->register_widget_type( new ColorMag_Elementor_Widgets_Grid_5() );
 
 		// 3. Global Widgets.
 		$widgets_manager->register_widget_type( new ColorMag_Elementor_Global_Widgets_Title() );
