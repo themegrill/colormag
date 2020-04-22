f8197763c3683543c0c80425233f9555b2c67fe0
Bishal Napit <mebishalnapit@gmail.com>
2020-04-22 07:19:07 +0545

    Tweak - Update elementor functions file required for this theme

--- inc/elementor/elementor-functions.php
+++ inc/elementor/elementor-functions.php
@@ -1,50 +1,54 @@
 <?php
 /**
- * Custom functions to be used within Elementor plugin
+ * Custom functions to be used within Elementor plugin.
  *
  * @package    ThemeGrill
  * @subpackage ColorMag
  * @since      ColorMag 1.2.3
  */
 
-/**
- * Return the values of all the categories of the posts
- * present in the site
- *
- * @return array of category ids and its respective names
- *
- * @since ColorMag 1.2.3
- */
-function colormag_elementor_categories() {
-	$output     = array();
-	$categories = get_categories();
+if ( ! function_exists( 'colormag_elementor_categories' ) ) :
 
-	foreach ( $categories as $category ) {
-		$output[ $category->term_id ] = $category->name;
+	/**
+	 * Return the values of all the categories of the posts present in the site.
+	 *
+	 * @return array of category ids and its respective names.
+	 *
+	 * @since ColorMag 1.2.3
+	 */
+	function colormag_elementor_categories() {
+		$output     = array();
+		$categories = get_categories();
+
+		foreach ( $categories as $category ) {
+			$output[ $category->term_id ] = $category->name;
+		}
+
+		return $output;
 	}
 
-	return $output;
-}
+endif;
 
 /**
- * Colormag_Elementor required setups
- * Particularly used for registering post thumbnail size and others
+ * Colormag_Elementor required setups.
  *
- * Hooked in after_setup_theme
+ * Particularly used for registering post thumbnail size and others.
+ *
+ * Hooked in after_setup_theme.
  *
  * @since ColorMag 2.2.3
  */
 function colormag_elementor_setup() {
 
-	// Cropping the images to different sizes to be used in the theme for Elementor
-	// For the block widgets
+	// Cropping the images to different sizes to be used in the theme for Elementor.
+	// For the block widgets.
 	add_image_size( 'colormag-elementor-block-extra-large-thumbnail', 1155, 480, true );
 
-	// Cropping the images to different sizes to be used in the theme for Elementor
-	// For the grid widgets
+	// For the grid widgets.
 	add_image_size( 'colormag-elementor-grid-large-thumbnail', 600, 417, true );
 	add_image_size( 'colormag-elementor-grid-small-thumbnail', 285, 450, true );
 	add_image_size( 'colormag-elementor-grid-medium-large-thumbnail', 575, 198, true );
+
 }
 
 add_action( 'after_setup_theme', 'colormag_elementor_setup' );
@@ -61,11 +65,16 @@ if ( ! function_exists( 'colormag_elementor_widgets_meta' ) ) :
 
 		<div class="tg-module-meta">
 			<?php
+			// Displays the same published and updated date if the post is never updated.
 			$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
+
+			// Displays the different published and updated date if the post is updated.
 			if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
 				$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
 			}
-			$time_string = sprintf( $time_string,
+
+			$time_string = sprintf(
+				$time_string,
 				esc_attr( get_the_date( 'c' ) ),
 				esc_html( get_the_date() ),
 				esc_attr( get_the_modified_date( 'c' ) ),
@@ -74,7 +83,7 @@ if ( ! function_exists( 'colormag_elementor_widgets_meta' ) ) :
 			?>
 
 			<span class="tg-post-date entry-date">
-				<?php echo '<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'; ?>
+				<?php echo '<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'; // phpcs:ignore WordPress.XSS.EscapeOutput.OutputNotEscaped ?>
 			</span>
 
 			<span class="tg-post-auther-name author vcard">
@@ -93,10 +102,10 @@ if ( ! function_exists( 'colormag_elementor_widgets_meta' ) ) :
 
 endif;
 
-if ( ! function_exists( 'colormag_elementor_styles' ) ) {
+if ( ! function_exists( 'colormag_elementor_styles' ) ) :
 
 	/**
-	 * Loads styles on elementor editor
+	 * Loads styles on elementor editor.
 	 *
 	 * @since ColorMag 1.2.3
 	 */
@@ -104,7 +113,7 @@ if ( ! function_exists( 'colormag_elementor_styles' ) ) {
 		wp_enqueue_style( 'colormag-econs', get_template_directory_uri() . '/inc/elementor/assets/css/colormag-econs.css', false, '1.0' );
 	}
 
-}
+endif;
 
 add_action( 'elementor/editor/after_enqueue_styles', 'colormag_elementor_styles' );
 
@@ -112,7 +121,11 @@ add_action( 'elementor/editor/after_enqueue_styles', 'colormag_elementor_styles'
 if ( ! function_exists( 'colormag_elementor_colored_category' ) ) :
 
 	/**
-	 * Display/Returns the category names for Elementor widgets
+	 * Display/Returns the category names for Elementor widgets.
+	 *
+	 * @param bool $display The option to display or echo the value.
+	 *
+	 * @return mixed
 	 *
 	 * @since ColorMag 1.2.3
 	 */
@@ -134,15 +147,15 @@ if ( ! function_exists( 'colormag_elementor_colored_category' ) ) :
 			}
 			$output .= '</div>';
 
-			if ( $display == 0 ) {
+			if ( 0 == $display ) {
 				$output = trim( $output, $separator );
 			}
-			if ( $display == 1 ) {
-				echo trim( $output, $separator );
+			if ( 1 == $display ) {
+				echo trim( $output, $separator ); // phpcs:ignore WordPress.XSS.EscapeOutput.OutputNotEscaped
 			}
 		}
 
-		if ( $display == 0 ) {
+		if ( 0 == $display ) {
 			return $output;
 		}
 	}
