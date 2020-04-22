81b94d4b565173afbae2b69259df7ae58800c7c6
Bishal Napit <mebishalnapit@gmail.com>
2020-04-22 14:00:58 +0545

    Tweak - Update render function for block style 9 widget

--- inc/elementor/widgets/colormag-elementor-widgets-block-9.php
+++ inc/elementor/widgets/colormag-elementor-widgets-block-9.php
@@ -81,55 +81,36 @@ class ColorMag_Elementor_Widgets_Block_9 extends Colormag_Elementor_Widget_Base 
 	 */
 	protected function render() {
 
+		$widget_title        = $this->get_settings( 'widget_title' );
 		$posts_number        = $this->get_settings( 'posts_number' );
 		$display_type        = $this->get_settings( 'display_type' );
 		$offset_posts_number = $this->get_settings( 'offset_posts_number' );
 		$categories_selected = $this->get_settings( 'categories_selected' );
 
-		$args = array(
-			'posts_per_page'      => $posts_number,
-			'post_type'           => 'post',
-			'ignore_sticky_posts' => true,
-			'no_found_rows'       => true,
-		);
-
-		// Display from the category selected
-		if ( 'categories' == $display_type ) {
-			$args[ 'category__in' ] = $categories_selected;
-		}
-
-		// Offset the posts
-		if ( ! empty( $offset_posts_number ) ) {
-			$args[ 'offset' ] = $offset_posts_number;
-		}
-
-		// Start the WP_Query Object/Class
-		$get_featured_posts = new \WP_Query( $args );
+		// Create the posts query.
+		$get_featured_posts = $this->query_posts( $posts_number, $display_type, $categories_selected,$offset_posts_number );
 		?>
 
 		<div class="tg-module-block tg-module-block--style-9 no-module-thub tg-module-wrapper">
 			<?php
-			$widget_title = $this->get_settings( 'widget_title' );
-			if ( ! empty( $widget_title ) ) : ?>
-				<div class="tg-module-title-wrap">
-					<h4 class="module-title">
-						<span><?php echo $this->get_settings( 'widget_title' ); ?></span>
-					</h4>
-				</div>
-			<?php endif;
+			// Displays the widget title.
+			$this->widget_title( $widget_title );
 			?>
 
 			<div class="tg-row">
 				<?php
 				while ( $get_featured_posts->have_posts() ) :
-					$get_featured_posts->the_post(); ?>
+					$get_featured_posts->the_post();
+					?>
+
 					<div class="tg_module_block tg_module_block--list-small no-thumbnail tg-col-control">
 						<div class="tg-module-info">
-							<h3 class="tg-module-title entry-title">
-								<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-							</h3>
+							<?php
+							// Display the post title.
+							$this->the_title();
 
-							<?php colormag_elementor_widgets_meta(); // Displays the entry meta
+							// Displays the entry meta.
+							colormag_elementor_widgets_meta();
 							?>
 						</div>
 					</div>
@@ -137,7 +118,7 @@ class ColorMag_Elementor_Widgets_Block_9 extends Colormag_Elementor_Widget_Base 
 					<?php
 				endwhile;
 
-				// Reset the postdata
+				// Reset the postdata.
 				wp_reset_postdata();
 				?>
 			</div>
@@ -145,4 +126,5 @@ class ColorMag_Elementor_Widgets_Block_9 extends Colormag_Elementor_Widget_Base 
 
 		<?php
 	}
+
 }
