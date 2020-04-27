c17974f92a2ff4c40376811035534d3aee40935f
Bishal Napit <mebishalnapit@gmail.com>
2020-04-27 12:33:23 +0545

    Tweak - Fix translators for TGMPA notices

--- inc/tgm-plugin-activation/tgmpa-colormag.php
+++ inc/tgm-plugin-activation/tgmpa-colormag.php
@@ -1,9 +1,10 @@
 <?php
 /**
- * This file contains the recommended plugin lists to this theme
+ * This file contains the recommended plugin lists to this theme.
+ *
+ * @package    ThemeGrill
+ * @subpackage ColorMag
  */
-
-add_action( 'tgmpa_register', 'colormag_register_required_plugins' );
 
 /**
  * Register the recommended plugins for this theme.
@@ -54,43 +55,43 @@ function colormag_register_required_plugins() {
 		'strings' => array(
 			'page_title'                      => esc_html__( 'Install Required Plugins', 'colormag' ),
 			'menu_title'                      => esc_html__( 'Install Plugins', 'colormag' ),
-			/* translators: %s: plugin name. */
+			/* Translators: %s: plugin name. */
 			'installing'                      => esc_html__( 'Installing Plugin: %s', 'colormag' ),
-			/* translators: %s: plugin name. */
+			/* Translators: %s: plugin name. */
 			'updating'                        => esc_html__( 'Updating Plugin: %s', 'colormag' ),
 			'oops'                            => esc_html__( 'Something went wrong with the plugin API.', 'colormag' ),
+			/* Translators: 1: plugin name(s). */
 			'notice_can_install_required'     => _n_noop(
-			/* translators: 1: plugin name(s). */
 				'This theme requires the following plugin: %1$s.',
 				'This theme requires the following plugins: %1$s.',
 				'colormag'
 			),
+			/* Translators: 1: plugin name(s). */
 			'notice_can_install_recommended'  => _n_noop(
-			/* translators: 1: plugin name(s). */
 				'This theme recommends the following plugin: %1$s.',
 				'This theme recommends the following plugins: %1$s.',
 				'colormag'
 			),
+			/* Translators: 1: plugin name(s). */
 			'notice_ask_to_update'            => _n_noop(
-			/* translators: 1: plugin name(s). */
 				'The following plugin needs to be updated to its latest version to ensure maximum compatibility with this theme: %1$s.',
 				'The following plugins need to be updated to their latest version to ensure maximum compatibility with this theme: %1$s.',
 				'colormag'
 			),
+			/* Translators: 1: plugin name(s). */
 			'notice_ask_to_update_maybe'      => _n_noop(
-			/* translators: 1: plugin name(s). */
 				'There is an update available for: %1$s.',
 				'There are updates available for the following plugins: %1$s.',
 				'colormag'
 			),
+			/* Translators: 1: plugin name(s). */
 			'notice_can_activate_required'    => _n_noop(
-			/* translators: 1: plugin name(s). */
 				'The following required plugin is currently inactive: %1$s.',
 				'The following required plugins are currently inactive: %1$s.',
 				'colormag'
 			),
+			/* Translators: 1: plugin name(s). */
 			'notice_can_activate_recommended' => _n_noop(
-			/* translators: 1: plugin name(s). */
 				'The following recommended plugin is currently inactive: %1$s.',
 				'The following recommended plugins are currently inactive: %1$s.',
 				'colormag'
@@ -113,11 +114,11 @@ function colormag_register_required_plugins() {
 			'return'                          => esc_html__( 'Return to Required Plugins Installer', 'colormag' ),
 			'plugin_activated'                => esc_html__( 'Plugin activated successfully.', 'colormag' ),
 			'activated_successfully'          => esc_html__( 'The following plugin was activated successfully:', 'colormag' ),
-			/* translators: 1: plugin name. */
+			/* Translators: 1: plugin name. */
 			'plugin_already_active'           => esc_html__( 'No action taken. Plugin %1$s was already active.', 'colormag' ),
-			/* translators: 1: plugin name. */
+			/* Translators: 1: plugin name. */
 			'plugin_needs_higher_version'     => esc_html__( 'Plugin not activated. A higher version of %s is needed for this theme. Please update the plugin.', 'colormag' ),
-			/* translators: 1: dashboard link. */
+			/* Translators: 1: dashboard link. */
 			'complete'                        => esc_html__( 'All plugins installed and activated successfully. %1$s', 'colormag' ),
 			'dismiss'                         => esc_html__( 'Dismiss this notice', 'colormag' ),
 			'notice_cannot_install_activate'  => esc_html__( 'There are one or more required or recommended plugins to install, update or activate.', 'colormag' ),
@@ -131,3 +132,5 @@ function colormag_register_required_plugins() {
 
 	tgmpa( $plugins, $config );
 }
+
+add_action( 'tgmpa_register', 'colormag_register_required_plugins' );
