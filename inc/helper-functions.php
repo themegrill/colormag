09a9dadd6bdaaa9331be40af24b0cb3362c2e19e
Bishal Napit <mebishalnapit@gmail.com>
2020-04-29 11:29:05 +0545

    Tweak - Add helper function to return class to be used in body_class filter

--- inc/helper-functions.php
+++ inc/helper-functions.php
@@ -31,3 +31,28 @@ if ( ! function_exists( 'colormag_elementor_active_page_check' ) ) :
 	}
 
 endif;
+
+
+if ( ! function_exists( 'colormag_sidebar_layout_class' ) ) :
+
+	/**
+	 * Returns the class for sidebar layout.
+	 *
+	 * @param string $option Option value.
+	 *
+	 * @return string Required class for sidebar layout to be used on body class.
+	 */
+	function colormag_sidebar_layout_class( $option ) {
+
+		$output = str_replace( '_', '-', $option );
+
+		// This extra hack is needed since our theme does not have the class of `.no-sidebar-content-centered`.
+		if ( 'no-sidebar-content-centered' === $output ) {
+			$output = 'no-sidebar';
+		}
+
+		return $output;
+
+	}
+
+endif;
