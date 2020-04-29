4885d28ab681b467a7c91f5d1d40b4560e6cbe18
Bishal Napit <mebishalnapit@gmail.com>
2020-04-29 10:39:05 +0545

    Tweak - Add PHPDoc Block for WooCommerce wrappers

--- inc/woocommerce.php
+++ inc/woocommerce.php
@@ -34,10 +34,12 @@ function colormag_woocommerce_setup() {
 add_action( 'after_setup_theme', 'colormag_woocommerce_setup' );
 
 /**
- * Making the theme Woocommrece compatible
+ * Remove default WooCommerce wrapper.
  */
 remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10 );
 remove_action( 'woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10 );
+
+// Remove default WooCommerce breadcrumb.
 remove_action( 'woocommerce_before_main_content', 'woocommerce_breadcrumb', 20, 0 );
 
 // Remove WooCommerce default sidebar.
@@ -46,14 +48,30 @@ remove_action( 'woocommerce_sidebar', 'woocommerce_get_sidebar', 10 );
 // Disable the page title display for WooCommerce pages.
 add_filter( 'woocommerce_show_page_title', '__return_false' );
 
-add_action( 'woocommerce_before_main_content', 'colormag_wrapper_start', 10 );
-add_action( 'woocommerce_after_main_content', 'colormag_wrapper_end', 10 );
-
+/**
+ * Before Content.
+ *
+ * Wraps all WooCommerce content in wrappers which match the theme markup.
+ *
+ * @return void
+ */
 function colormag_wrapper_start() {
 	echo '<div id="primary">';
 }
 
+add_action( 'woocommerce_before_main_content', 'colormag_wrapper_start', 10 );
+
+/**
+ * After Content.
+ *
+ * Closes the wrapping divs.
+ *
+ * @return void
+ */
 function colormag_wrapper_end() {
 	echo '</div>';
+
 	colormag_sidebar_select();
 }
+
+add_action( 'woocommerce_after_main_content', 'colormag_wrapper_end', 10 );
