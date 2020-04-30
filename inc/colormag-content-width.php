14cb88756735b7a780b087002cc58b2339518511
Bishal Napit <mebishalnapit@gmail.com>
2020-04-30 07:53:02 +0545

    Tweak - Set content width via hook

--- inc/colormag-content-width.php
+++ inc/colormag-content-width.php
@@ -13,11 +13,22 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 /**
- * Set the content width based on the theme's design and stylesheet.
+ * Set the content width in pixels, based on the theme's design and stylesheet.
+ *
+ * Priority 0 to make it available to lower priority callbacks.
+ *
+ * @global int $content_width
  */
-if ( ! isset( $content_width ) ) {
-	$content_width = 800;
+function colormag_set_content_width() {
+
+	// This variable is intended to be overruled from themes.
+	// Open WPCS issue: {@link https://github.com/WordPress-Coding-Standards/WordPress-Coding-Standards/issues/1043}.
+	// phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedVariableFound
+	$GLOBALS['content_width'] = apply_filters( 'colormag_set_content_width', 800 );
+
 }
+
+add_action( 'after_setup_theme', 'colormag_set_content_width', 0 );
 
 /**
  * $content_width global variable adjustment as per layout option.
