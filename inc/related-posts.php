e4e1825e3e1fad7178203d3ecf1f756e88ef216e
Bishal Napit <mebishalnapit@gmail.com>
2020-05-04 11:54:48 +0545

    Tweak - Reformat related posts template file

--- inc/related-posts.php
+++ inc/related-posts.php
@@ -1,36 +1,63 @@
-<?php $related_posts = colormag_related_posts_function(); ?>
+<?php
+/**
+ * Related posts featured display..
+ *
+ * @package    ThemeGrill
+ * @subpackage ColorMag
+ * @since      ColorMag 1.0.0
+ */
 
-<?php if ( $related_posts->have_posts() ): ?>
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
 
-	<h4 class="related-posts-main-title">
-		<i class="fa fa-thumbs-up"></i><span><?php esc_html_e( 'You May Also Like', 'colormag' ); ?></span>
-	</h4>
+$related_posts = colormag_related_posts_function();
 
-	<div class="related-posts clearfix">
+if ( $related_posts->have_posts() ) :
+	?>
 
-		<?php while ( $related_posts->have_posts() ) : $related_posts->the_post(); ?>
-			<div class="single-related-posts">
+	<div class="related-posts-wrapper">
 
-				<?php if ( has_post_thumbnail() ): ?>
-					<div class="related-posts-thumbnail">
-						<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
-							<?php the_post_thumbnail( 'colormag-featured-post-medium' ); ?>
-						</a>
+		<h4 class="related-posts-main-title">
+			<i class="fa fa-thumbs-up"></i><span><?php esc_html_e( 'You May Also Like', 'colormag' ); ?></span>
+		</h4>
+
+		<div class="related-posts clearfix">
+
+			<?php
+			while ( $related_posts->have_posts() ) :
+				$related_posts->the_post();
+				?>
+				<div class="single-related-posts">
+
+					<?php if ( has_post_thumbnail() ) : ?>
+						<div class="related-posts-thumbnail">
+							<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
+								<?php the_post_thumbnail( 'colormag-featured-post-medium' ); ?>
+							</a>
+						</div>
+					<?php endif; ?>
+
+					<div class="article-content">
+						<h3 class="entry-title">
+							<a href="<?php the_permalink(); ?>" rel="bookmark" title="<?php the_title_attribute(); ?>">
+								<?php the_title(); ?>
+							</a>
+						</h3><!--/.post-title-->
+
+						<?php colormag_entry_meta( false ); ?>
 					</div>
-				<?php endif; ?>
 
-				<div class="article-content">
-					<h3 class="entry-title">
-						<a href="<?php the_permalink(); ?>" rel="bookmark" title="<?php the_title(); ?>"><?php the_title(); ?></a>
-					</h3><!--/.post-title-->
+				</div><!--/.related-->
+			<?php endwhile; ?>
 
-					<?php colormag_entry_meta( false ); ?>
-				</div>
+		</div><!--/.post-related-->
 
-			</div><!--/.related-->
-		<?php endwhile; ?>
+	</div>
 
-	</div><!--/.post-related-->
-<?php endif; ?>
+	<?php
+endif;
 
-<?php wp_reset_postdata(); ?>
+// Reset postdata.
+wp_reset_postdata();
