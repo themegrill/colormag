1fba0f60d71602064fb6ac5537f78dc698253418
Bishal Napit <mebishalnapit@gmail.com>
2020-05-06 16:48:39 +0545

    Tweak - Least priority for comment navigation heading

--- comments.php
+++ comments.php
@@ -52,7 +52,7 @@ if ( post_password_required() ) {
 
 		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : ?>
 			<nav id="comment-nav-above" class="comment-navigation clearfix" role="navigation">
-				<h3 class="screen-reader-text"><?php esc_html_e( 'Comment navigation', 'colormag' ); ?></h3>
+				<h4 class="screen-reader-text"><?php esc_html_e( 'Comment navigation', 'colormag' ); ?></h4>
 
 				<div class="nav-previous"><?php previous_comments_link( esc_html__( '&larr; Older Comments', 'colormag' ) ); ?></div>
 				<div class="nav-next"><?php next_comments_link( esc_html__( 'Newer Comments &rarr;', 'colormag' ) ); ?></div>
@@ -72,7 +72,7 @@ if ( post_password_required() ) {
 
 		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : ?>
 			<nav id="comment-nav-below" class="comment-navigation clearfix" role="navigation">
-				<h3 class="screen-reader-text"><?php esc_html_e( 'Comment navigation', 'colormag' ); ?></h3>
+				<h4 class="screen-reader-text"><?php esc_html_e( 'Comment navigation', 'colormag' ); ?></h4>
 
 				<div class="nav-previous"><?php previous_comments_link( esc_html__( '&larr; Older Comments', 'colormag' ) ); ?></div>
 				<div class="nav-next"><?php next_comments_link( esc_html__( 'Newer Comments &rarr;', 'colormag' ) ); ?></div>
