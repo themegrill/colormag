46621b95778783fba51962a41dccb751973b507a
Bishal Napit <mebishalnapit@gmail.com>
2020-05-06 16:42:02 +0545

    Tweak - Similar heading tag

--- comments.php
+++ comments.php
@@ -52,7 +52,7 @@ if ( post_password_required() ) {
 
 		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : ?>
 			<nav id="comment-nav-above" class="comment-navigation clearfix" role="navigation">
-				<h4 class="screen-reader-text"><?php esc_html_e( 'Comment navigation', 'colormag' ); ?></h4>
+				<h3 class="screen-reader-text"><?php esc_html_e( 'Comment navigation', 'colormag' ); ?></h3>
 
 				<div class="nav-previous"><?php previous_comments_link( esc_html__( '&larr; Older Comments', 'colormag' ) ); ?></div>
 				<div class="nav-next"><?php next_comments_link( esc_html__( 'Newer Comments &rarr;', 'colormag' ) ); ?></div>
