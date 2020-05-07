02c586fbb522c03f0c2bb83c26643edb1cc7547d
Bishal Napit <mebishalnapit@gmail.com>
2020-05-07 09:23:46 +0545

    Tweak - Add required hooks for page.php file

--- page.php
+++ page.php
@@ -22,21 +22,39 @@ do_action( 'colormag_before_body_content' );
 
 	<div id="primary">
 		<div id="content" class="clearfix">
-			<?php while ( have_posts() ) : the_post(); ?>
+			<?php
+			/**
+			 * Hook: colormag_before_single_page_loop.
+			 */
+			do_action( 'colormag_before_single_page_loop' );
 
-				<?php get_template_part( 'content', 'page' ); ?>
+			while ( have_posts() ) :
+				the_post();
 
-				<?php
+				get_template_part( 'content', 'page' );
+
+				/**
+				 * Hook: colormag_before_comments_template.
+				 */
 				do_action( 'colormag_before_comments_template' );
-				// If comments are open or we have at least one comment, load up the comment template
+
+				// If comments are open or we have at least one comment, load up the comment template.
 				if ( comments_open() || '0' != get_comments_number() ) {
 					comments_template();
 				}
+
+				/**
+				 * Hook: colormag_after_comments_template.
+				 */
 				do_action( 'colormag_after_comments_template' );
-				?>
 
-			<?php endwhile; ?>
+			endwhile;
 
+			/**
+			 * Hook: colormag_after_single_page_loop.
+			 */
+			do_action( 'colormag_after_single_page_loop' );
+			?>
 		</div><!-- #content -->
 	</div><!-- #primary -->
 
