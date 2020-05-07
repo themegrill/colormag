b2cee1d0a56653ae83846484682939cd88e9ab47
Bishal Napit <mebishalnapit@gmail.com>
2020-05-07 09:45:03 +0545

    Tweak - Add preexisting hook docs on single.php file

--- single.php
+++ single.php
@@ -2,13 +2,23 @@
 /**
  * Theme Single Post Section for our theme.
  *
- * @package ThemeGrill
+ * @package    ThemeGrill
  * @subpackage ColorMag
- * @since ColorMag 1.0
+ * @since      ColorMag 1.0
  */
-get_header(); ?>
 
-	<?php do_action( 'colormag_before_body_content' ); ?>
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+get_header();
+
+/**
+ * Hook: colormag_before_body_content.
+ */
+do_action( 'colormag_before_body_content' );
+?>
 
 	<div id="primary">
 		<div id="content" class="clearfix">
@@ -21,32 +31,38 @@ get_header(); ?>
 
 		</div><!-- #content -->
 
-      <?php get_template_part( 'navigation', 'single' ); ?>
+		<?php get_template_part( 'navigation', 'single' ); ?>
 
-      <?php if ( get_the_author_meta( 'description' ) ) : ?>
-         <div class="author-box">
-            <div class="author-img"><?php echo get_avatar( get_the_author_meta( 'user_email' ), '100' ); ?></div>
-               <h4 class="author-name"><?php the_author_meta( 'display_name' ); ?></h4>
-               <p class="author-description"><?php the_author_meta( 'description' ); ?></p>
-         </div>
-      <?php endif; ?>
+		<?php if ( get_the_author_meta( 'description' ) ) : ?>
+			<div class="author-box">
+				<div class="author-img"><?php echo get_avatar( get_the_author_meta( 'user_email' ), '100' ); ?></div>
+				<h4 class="author-name"><?php the_author_meta( 'display_name' ); ?></h4>
+				<p class="author-description"><?php the_author_meta( 'description' ); ?></p>
+			</div>
+		<?php endif; ?>
 
-      <?php if ( get_theme_mod( 'colormag_related_posts_activate', 0 ) == 1 )
-         get_template_part( 'inc/related-posts' );
-      ?>
+		<?php if ( get_theme_mod( 'colormag_related_posts_activate', 0 ) == 1 ) {
+			get_template_part( 'inc/related-posts' );
+		}
+		?>
 
-      <?php
-         do_action( 'colormag_before_comments_template' );
-         // If comments are open or we have at least one comment, load up the comment template
-         if ( comments_open() || '0' != get_comments_number() )
-            comments_template();
-         do_action ( 'colormag_after_comments_template' );
-      ?>
+		<?php
+		do_action( 'colormag_before_comments_template' );
+		// If comments are open or we have at least one comment, load up the comment template
+		if ( comments_open() || '0' != get_comments_number() ) {
+			comments_template();
+		}
+		do_action( 'colormag_after_comments_template' );
+		?>
 
 	</div><!-- #primary -->
 
-	<?php colormag_sidebar_select(); ?>
+<?php
+colormag_sidebar_select();
 
-	<?php do_action( 'colormag_after_body_content' ); ?>
+/**
+ * Hook: colormag_after_body_content.
+ */
+do_action( 'colormag_after_body_content' );
 
-<?php get_footer(); ?>
+get_footer();
