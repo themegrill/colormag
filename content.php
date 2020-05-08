5bcb67cfaa16759644ca2ce497d3bf7de7cef973
Bishal Napit <mebishalnapit@gmail.com>
2020-05-08 06:17:30 +0545

    Tweak - Reformat content-page.php file

--- content.php
+++ content.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The template used for displaying page content in page.php
+ * The template used for displaying page content in archive pages.
  *
  * @package    ThemeGrill
  * @subpackage ColorMag
