6279604ad7c4b5daa01d8b2020cc51734bb9d5fb
Bishal Napit <mebishalnapit@gmail.com>
2020-05-12 08:53:12 +0545

    Tweak - Move header logo/title display option to header main area

--- inc/customizer/options/header/class-colormag-customize-header-main-area.php
+++ inc/customizer/options/header/class-colormag-customize-header-main-area.php
@@ -31,7 +31,25 @@ class ColorMag_Customize_Header_Main_Area extends ColorMag_Customize_Base_Option
 	 */
 	public function customizer_options( $options, $wp_customize ) {
 
-		$configs = array();
+		$configs = array(
+
+			// Header logo placement option.
+			array(
+				'name'    => 'colormag_header_logo_placement',
+				'default' => 'header_text_only',
+				'type'    => 'control',
+				'control' => 'radio',
+				'label'   => esc_html__( 'Choose the option that you want', 'colormag' ),
+				'section' => 'title_tagline',
+				'choices' => array(
+					'header_logo_only' => esc_html__( 'Header Logo Only', 'colormag' ),
+					'header_text_only' => esc_html__( 'Header Text Only', 'colormag' ),
+					'show_both'        => esc_html__( 'Show Both', 'colormag' ),
+					'disable'          => esc_html__( 'Disable', 'colormag' ),
+				),
+			),
+
+		);
 
 		$options = array_merge( $options, $configs );
 
