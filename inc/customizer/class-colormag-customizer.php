0d4ed95a27dc2d0d0803449fac8816e8fcd84f00
Bishal Napit <mebishalnapit@gmail.com>
2020-05-12 09:59:54 +0545

    Tweak - Add required file for primary menu option

--- inc/customizer/class-colormag-customizer.php
+++ inc/customizer/class-colormag-customizer.php
@@ -471,6 +471,7 @@ class ColorMag_Customizer {
 		require COLORMAG_CUSTOMIZER_DIR . '/options/header/class-colormag-customize-header-top-bar.php';
 		require COLORMAG_CUSTOMIZER_DIR . '/options/header/class-colormag-customize-header-main-area.php';
 		require COLORMAG_CUSTOMIZER_DIR . '/options/header/class-colormag-customize-header-media.php';
+		require COLORMAG_CUSTOMIZER_DIR . '/options/header/class-colormag-customize-primary-menu.php';
 
 		require COLORMAG_CUSTOMIZER_DIR . '/options/header/class-colormag-customize-header-options.php';
 		require COLORMAG_CUSTOMIZER_DIR . '/options/design/class-colormag-customize-design-options.php';
