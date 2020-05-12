1658c4a931e9e66ea7696a62ac2d9fb09eda436b
Bishal Napit <mebishalnapit@gmail.com>
2020-05-12 13:41:32 +0545

    Tweak - Add required file for design general options

--- inc/customizer/class-colormag-customizer.php
+++ inc/customizer/class-colormag-customizer.php
@@ -469,6 +469,9 @@ class ColorMag_Customizer {
 		// General customize options.
 		require COLORMAG_CUSTOMIZER_DIR . '/options/general/class-colormag-customize-general-options.php';
 
+		// Design customize options.
+		require COLORMAG_CUSTOMIZER_DIR . '/options/design/class-colormag-customize-design-general-options.php';
+
 		// Header customize options.
 		require COLORMAG_CUSTOMIZER_DIR . '/options/header/class-colormag-customize-header-general-options.php';
 		require COLORMAG_CUSTOMIZER_DIR . '/options/header/class-colormag-customize-header-top-bar-options.php';
