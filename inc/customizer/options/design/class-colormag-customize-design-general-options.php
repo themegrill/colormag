8f05bb09f2a028d2f9a039c06b273b946d520374
Bishal Napit <mebishalnapit@gmail.com>
2020-05-12 14:02:33 +0545

    Tweak - Move default layout for pages only option inside general design options section

--- inc/customizer/options/design/class-colormag-customize-design-general-options.php
+++ inc/customizer/options/design/class-colormag-customize-design-general-options.php
@@ -63,6 +63,36 @@ class ColorMag_Customize_Design_General_Options  extends ColorMag_Customize_Base
 				'priority'  => 5,
 			),
 
+			// Default layout for pages only option.
+			array(
+				'name'      => 'colormag_default_page_layout',
+				'default'   => 'right_sidebar',
+				'type'      => 'control',
+				'control'   => 'colormag-radio-image',
+				'label'     => esc_html__( 'Default layout for pages only', 'colormag' ),
+				'section'   => 'colormag_design_general_section',
+				'choices'   => array(
+					'right_sidebar'               => array(
+						'label' => '',
+						'url'   => COLORMAG_ADMIN_IMAGES_URL . '/right-sidebar.png',
+					),
+					'left_sidebar'                => array(
+						'label' => '',
+						'url'   => COLORMAG_ADMIN_IMAGES_URL . '/left-sidebar.png',
+					),
+					'no_sidebar_full_width'       => array(
+						'label' => '',
+						'url'   => COLORMAG_ADMIN_IMAGES_URL . '/no-sidebar-full-width-layout.png',
+					),
+					'no_sidebar_content_centered' => array(
+						'label' => '',
+						'url'   => COLORMAG_ADMIN_IMAGES_URL . '/no-sidebar-content-centered-layout.png',
+					),
+				),
+				'image_col' => 2,
+				'priority'  => 10,
+			),
+
 		);
 
 		$options = array_merge( $options, $configs );
