f13d90f67e51cbb269ed9950c0c0f508cd61b326
Bishal Napit <mebishalnapit@gmail.com>
2020-05-13 10:50:03 +0545

    Tweak - Add required file for single post customize options

--- inc/customizer/class-colormag-customizer.php
+++ inc/customizer/class-colormag-customizer.php
@@ -479,6 +479,9 @@ class ColorMag_Customizer {
 		// Design customize options.
 		require COLORMAG_CUSTOMIZER_DIR . '/options/design/class-colormag-customize-design-general-options.php';
 
+		// Post/Page/Blog Options.
+		require COLORMAG_CUSTOMIZER_DIR . '/options/blog/class-colormag-customize-blog-single-post-options.php';
+
 		require COLORMAG_CUSTOMIZER_DIR . '/options/design/class-colormag-customize-design-options.php';
 		require COLORMAG_CUSTOMIZER_DIR . '/options/social/class-colormag-customize-social-options.php';
 		require COLORMAG_CUSTOMIZER_DIR . '/options/footer/class-colormag-customize-footer-options.php';
