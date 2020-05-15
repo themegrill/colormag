74d9255962b0c01a961772f3617cfda64a71170c
Bishal Napit <mebishalnapit@gmail.com>
2020-05-15 08:26:37 +0545

    Tweak - Add migration function for customize options migrate

--- inc/migration.php
+++ inc/migration.php
@@ -11,3 +11,23 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
+
+
+/**
+ * Migrate all of the customize options for 2.0.0 theme update.
+ *
+ * @since ColorMag 2.0.0
+ */
+function colormag_major_theme_update_customize_migrate() {
+
+	// Bail out if the migration is already done.
+	if ( get_option( 'colormag_major_theme_update_customize_migrate' ) ) {
+		return;
+	}
+
+	// Set flag to not repeat the migration process, ie, run it only once.
+	update_option( 'colormag_major_theme_update_customize_migrate', true );
+
+}
+
+add_action( 'after_setup_theme', 'colormag_major_theme_update_customize_migrate' );
