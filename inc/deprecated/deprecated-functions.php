31c8cce36784f8ede5a4731c981e7007101a7009
Bishal Napit <mebishalnapit@gmail.com>
2020-05-21 09:31:47 +0545

    Tweak - Deprecate radio/select sanitizations

--- inc/deprecated/deprecated-functions.php
+++ inc/deprecated/deprecated-functions.php
@@ -42,3 +42,92 @@ if ( ! function_exists( 'colormag_render_header_image' ) ) :
 	}
 
 endif;
+
+
+if ( ! function_exists( 'colormag_radio_select_sanitize' ) ) :
+
+	/**
+	 * Deprecate function for radio/select sanitization.
+	 *
+	 * @param string               $input   Input from the customize controls.
+	 * @param WP_Customize_Setting $setting Setting instance.
+	 */
+	function colormag_radio_select_sanitize( $input, $setting ) {
+
+		_deprecated_function( __FUNCTION__, '2.0.0', 'ColorMag_Customizer_Sanitizes::sanitize_radio_select( $input, $setting )' );
+
+		return ColorMag_Customizer_Sanitizes::sanitize_radio_select( $input, $setting );
+
+	}
+
+endif;
+
+
+if ( ! function_exists( 'colormag_related_posts_sanitize' ) ) :
+
+	/**
+	 * Deprecate function for radio/select sanitization.
+	 *
+	 * @param string $input Customizer input.
+	 */
+	function colormag_related_posts_sanitize( $input ) {
+		_deprecated_function( __FUNCTION__, '2.0.0' );
+	}
+
+endif;
+
+
+if ( ! function_exists( 'colormag_show_radio_saniztize' ) ) :
+
+	/**
+	 * Deprecate function for radio/select sanitization.
+	 *
+	 * @param string $input Customizer input.
+	 */
+	function colormag_show_radio_saniztize( $input ) {
+		_deprecated_function( __FUNCTION__, '2.0.0' );
+	}
+
+endif;
+
+
+if ( ! function_exists( 'colormag_header_image_position_sanitize' ) ) :
+
+	/**
+	 * Deprecate function for radio/select sanitization.
+	 *
+	 * @param string $input Customizer input.
+	 */
+	function colormag_header_image_position_sanitize( $input ) {
+		_deprecated_function( __FUNCTION__, '2.0.0' );
+	}
+
+endif;
+
+
+if ( ! function_exists( 'colormag_site_layout_sanitize' ) ) :
+
+	/**
+	 * Deprecate function for radio/select sanitization.
+	 *
+	 * @param string $input Customizer input.
+	 */
+	function colormag_site_layout_sanitize( $input ) {
+		_deprecated_function( __FUNCTION__, '2.0.0' );
+	}
+
+endif;
+
+
+if ( ! function_exists( 'colormag_layout_sanitize' ) ) :
+
+	/**
+	 * Deprecate function for radio/select sanitization.
+	 *
+	 * @param string $input Customizer input.
+	 */
+	function colormag_layout_sanitize( $input ) {
+		_deprecated_function( __FUNCTION__, '2.0.0' );
+	}
+
+endif;
