ccfa91c3eaaa51084f66618294b18755dbd56d44
ThemeGrill <milanthapa299@gmail.com>
2020-05-22 17:11:46 +0545

    Added correct text domain

--- inc/admin/class-colormag-welcome-notice.php
+++ inc/admin/class-colormag-welcome-notice.php
@@ -60,7 +60,7 @@ class Colormag_Welcome_Notice {
 
 					<div class="colormag-message__cta">
 						<?php echo $this->import_button_html(); ?>
-						<span class="plugin-install-notice"><?php esc_html_e( 'Clicking the button will install and activate the ThemeGrill demo importer plugin.', 'imalayas' ); ?></span>
+						<span class="plugin-install-notice"><?php esc_html_e( 'Clicking the button will install and activate the ThemeGrill demo importer plugin.', 'colormag' ); ?></span>
 					</div>
 				</div>
 			</div>
