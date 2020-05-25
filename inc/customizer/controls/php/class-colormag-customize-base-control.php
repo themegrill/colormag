96be7147c8761411e9ee7223adcda394cb06ef1c
ThemeGrill <milanthapa299@gmail.com>
2020-05-25 15:04:46 +0545

    Added customizer base control class

---
+++ inc/customizer/controls/php/class-colormag-customize-base-control.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * Customize Base control class.
+ *
+ * @package colormag
+ *
+ * @see     WP_Customize_Control
+ * @access  public
+ */
+
+/**
+ * Class ColorMag_Customize_Base_Control
+ */
+class ColorMag_Customize_Base_Control extends WP_Customize_Control {
+
+	/**
+	 * Enqueue scripts all controls.
+	 */
+	public function enqueue() {
+
+		// Scripts for nesting panel/section.
+		wp_enqueue_style( 'colormag-customize-upsell-section', COLORMAG_INCLUDES_URL . '/customizer/assets/css/colormag-customize-upsell-section.css' );
+
+	}
+
+
+	/**
+	 * Refresh the parameters passed to the JavaScript via JSON.
+	 *
+	 * @see    WP_Customize_Control::to_json()
+	 * @access public
+	 * @return void
+	 */
+	public function to_json() {
+
+		parent::to_json();
+
+		$this->json['default'] = $this->setting->default;
+		if ( isset( $this->default ) ) {
+			$this->json['default'] = $this->default;
+		}
+
+		$this->json['id']      = $this->id;
+		$this->json['value']   = $this->value();
+		$this->json['choices'] = $this->choices;
+		$this->json['link']    = $this->get_link();
+		$this->json['l10n']    = $this->l10n();
+
+		$this->json['inputAttrs'] = '';
+		foreach ( $this->input_attrs as $attr => $value ) {
+			$this->json['inputAttrs'] .= $attr . '="' . esc_attr( $value ) . '" ';
+		}
+
+	}
+
+	/**
+	 * Render content is still called, so be sure to override it with an empty function in your subclass as well.
+	 */
+	protected function render_content() {
+	}
+
+	/**
+	 * Renders the Underscore template for this control.
+	 *
+	 * @see    WP_Customize_Control::print_template()
+	 * @access protected
+	 * @return void
+	 */
+	protected function content_template() {
+	}
+
+	/**
+	 * Returns an array of translation strings.
+	 *
+	 * @access protected
+	 * @return array
+	 */
+	protected function l10n() {
+		return array();
+	}
+
+}
