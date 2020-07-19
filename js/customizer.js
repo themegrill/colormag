3c5870391576cc9264da47487d7ca50179cbb018
Ashish <ashishsthanp@gmail.com>
2020-07-19 18:37:42 +0545

    Fix - View Pro section not appearing

--- js/customizer.js
+++ js/customizer.js
@@ -138,21 +138,4 @@
 		);
 	} );
 
-	// Make clickable section possible.
-	(
-		function ( $, api ) {
-			api.sectionConstructor['colormag-upsell-section'] = api.Section.extend( {
-
-				// No events for this type of section.
-				attachEvents : function () {
-				},
-
-				// Always make the section active.
-				isContextuallyActive : function () {
-					return true;
-				}
-			} );
-		}
-	)( jQuery, wp.customize );
-
 } )( jQuery );
