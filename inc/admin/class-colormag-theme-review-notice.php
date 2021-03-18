30f9d8282a90983b23081a7d8c3ee58716fd25de
Premi Jimee <jimeepremi@gmail.com>
2021-03-18 09:59:31 +0545

    Fix - Minor issues

--- inc/admin/class-colormag-theme-review-notice.php
+++ inc/admin/class-colormag-theme-review-notice.php
@@ -57,7 +57,7 @@ class ColorMag_Theme_Review_Notice {
 		 * 2. If the user has ignored the message partially for 15 days.
 		 * 3. Dismiss always if clicked on 'I Already Did' button.
 		 */
-		if ( ( get_option( 'colormag_theme_installed_time' ) > strtotime( '0 day' ) ) || ( $ignored_notice_partially > strtotime( '-15 day' ) ) || ( $ignored_notice ) ) {
+		if ( ( get_option( 'colormag_theme_installed_time' ) > strtotime( '-15 day' ) ) || ( $ignored_notice_partially > strtotime( '-15 day' ) ) || ( $ignored_notice ) ) {
 			return;
 		}
 		?>
