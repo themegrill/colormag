594df49495b64a8ac41d9999fce3b53e0c3964a9
Premiii <jimeepremi@gmail.com>
2021-05-11 09:57:21 +0545

    Update - Upgrade notice more discount

--- inc/admin/class-colormag-upgrade-notice.php
+++ inc/admin/class-colormag-upgrade-notice.php
@@ -78,8 +78,8 @@ class ColorMag_Upgrade_Notice extends ColorMag_Notice {
 					),
 					'<strong>' . esc_html( $current_user->display_name ) . '</strong>',
 					'ColorMag',
-					'<code class="coupon-code">upgrade15</code>',
-					'15%'
+					'<code class="coupon-code">TGFREEUSER</code>',
+					'25%'
 				);
 				?>
 			</p>
