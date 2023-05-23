fd97b4536dedc19f2b5aad89a31410045a661a3c
Sakarr <ctha.sakar@gmail.com>
2023-05-23 17:11:00 +0545

    Remove - Typography CSS from customize preview.

--- inc/customizer/assets/js/cm-customize-preview.js
+++ inc/customizer/assets/js/cm-customize-preview.js
@@ -8,6 +8,3 @@
 
 colormagGenerateDimensionCSS( 'colormag_primary_menu_dimension_padding', '.cm-primary-nav a', 'padding' );
 colormagGenerateDimensionCSS( 'colormag_main_header_dimension_padding', '.cm-primary-nav', 'padding' );
-
-colormagGenerateTypographyCSS( 'colormag_primary_menu_typography', '.cm-primary-nav ul li a');
-colormagGenerateTypographyCSS( 'colormag_primary_sub_menu_typography', '.cm-primary-nav ul li ul li a');
