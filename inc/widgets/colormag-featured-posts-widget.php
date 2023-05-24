9c8d37090b179b6471d727ab7b32c672e7e2f789
Sakarr <ctha.sakar@gmail.com>
2023-05-24 14:06:34 +0545

    Update -  Featured Posts widget code according to free version.

--- inc/widgets/colormag-featured-posts-widget.php
+++ inc/widgets/colormag-featured-posts-widget.php
@@ -28,28 +28,28 @@ class colormag_featured_posts_widget extends ColorMag_Widget {
 		$this->widget_description = esc_html__( 'Display latest posts or posts of specific category.', 'colormag' );
 		$this->widget_name        = esc_html__( 'TG: Featured Posts (Style 1)', 'colormag' );
 		$this->settings           = array(
-			'widget_layout'   => array(
+			'widget_layout' => array(
 				'type'      => 'custom',
 				'default'   => '',
 				'label'     => esc_html__( 'Layout will be as below:', 'colormag' ),
 				'image_url' => get_template_directory_uri() . '/assets/img/style-1.jpg',
 			),
-			'title'           => array(
+			'title'         => array(
 				'type'    => 'text',
 				'default' => '',
 				'label'   => esc_html__( 'Title:', 'colormag' ),
 			),
-			'text'            => array(
+			'text'          => array(
 				'type'    => 'textarea',
 				'default' => '',
 				'label'   => esc_html__( 'Description', 'colormag' ),
 			),
-			'number'          => array(
+			'number'        => array(
 				'type'    => 'number',
 				'default' => 4,
 				'label'   => esc_html__( 'Number of posts to display:', 'colormag' ),
 			),
-			'type'            => array(
+			'type'          => array(
 				'type'    => 'radio',
 				'default' => 'latest',
 				'label'   => '',
@@ -60,35 +60,10 @@ class colormag_featured_posts_widget extends ColorMag_Widget {
 					'author'   => esc_html__( 'Show posts from an author', 'colormag' ),
 				),
 			),
-			'category'        => array(
+			'category'      => array(
 				'type'    => 'dropdown_categories',
 				'default' => '',
 				'label'   => esc_html__( 'Select category', 'colormag' ),
-			),
-			'tag'             => array(
-				'type'    => 'dropdown_tags',
-				'default' => '',
-				'label'   => esc_html__( 'Select tag', 'colormag' ),
-			),
-			'author'          => array(
-				'type'    => 'dropdown_users',
-				'default' => '',
-				'label'   => esc_html__( 'Select author', 'colormag' ),
-			),
-			'random_posts'    => array(
-				'type'    => 'checkbox',
-				'default' => '0',
-				'label'   => esc_html__( 'Check to display the random post from either the chosen category or from latest post.', 'colormag' ),
-			),
-			'child_category'  => array(
-				'type'    => 'checkbox',
-				'default' => '0',
-				'label'   => esc_html__( 'Check to display the posts from child category of the chosen category.', 'colormag' ),
-			),
-			'view_all_button' => array(
-				'type'    => 'checkbox',
-				'default' => '0',
-				'label'   => esc_html__( 'Check to display the view all button to link that button to the specific category chosen in this widget.', 'colormag' ),
 			),
 		);
 
@@ -107,36 +82,21 @@ class colormag_featured_posts_widget extends ColorMag_Widget {
 	public function widget( $args, $instance ) {
 
 		global $post;
-		$title           = apply_filters( 'widget_title', isset( $instance['title'] ) ? $instance['title'] : '' );
-		$text            = isset( $instance['text'] ) ? $instance['text'] : '';
-		$number          = empty( $instance['number'] ) ? 4 : $instance['number'];
-		$type            = isset( $instance['type'] ) ? $instance['type'] : 'latest';
-		$category        = isset( $instance['category'] ) ? $instance['category'] : '';
-		$random_posts    = ! empty( $instance['random_posts'] ) ? 'true' : 'false';
-		$child_category  = ! empty( $instance['child_category'] ) ? 'true' : 'false';
-		$tag             = isset( $instance['tag'] ) ? $instance['tag'] : '';
-		$author          = isset( $instance['author'] ) ? $instance['author'] : '';
-		$view_all_button = ! empty( $instance['view_all_button'] ) ? 'true' : 'false';
-
-		// For WPML plugin compatibility, register string.
-		if ( function_exists( 'icl_register_string' ) ) {
-			icl_register_string( 'ColorMag Pro', 'TG: Featured Posts (Style 1) Description' . $this->id, $text );
-		}
-
-		// For WPML plugin compatibility, assign variable to converted string.
-		if ( function_exists( 'icl_t' ) ) {
-			$text = icl_t( 'ColorMag Pro', 'TG: Featured Posts (Style 1) Description' . $this->id, $text );
-		}
+		$title    = apply_filters( 'widget_title', isset( $instance['title'] ) ? $instance['title'] : '' );
+		$text     = isset( $instance['text'] ) ? $instance['text'] : '';
+		$number   = empty( $instance['number'] ) ? 4 : $instance['number'];
+		$type     = isset( $instance['type'] ) ? $instance['type'] : 'latest';
+		$category = isset( $instance['category'] ) ? $instance['category'] : '';
 
 		// Create the posts query.
-		$get_featured_posts = $this->query_posts( $number, $type, $category, $tag, $author, $random_posts, $child_category );
+		$get_featured_posts = $this->query_posts( $number, $type, $category );
 
 		$this->widget_start( $args );
 		?>
 
 		<?php
 		// Displays the widget title.
-		$this->widget_title( $title, $type, $category, $view_all_button );
+		$this->widget_title( $title, $type, $category );
 
 		// Display the description.
 		$this->widget_description( $text );
@@ -163,19 +123,6 @@ class colormag_featured_posts_widget extends ColorMag_Widget {
 				if ( has_post_thumbnail() ) {
 					$this->the_post_thumbnail( $post->ID, $featured );
 				}
-
-				if ( has_post_format( 'video' ) && ! ( has_post_thumbnail() ) ) :
-					$video_post_url = get_post_meta( $post->ID, 'video_url', true );?>
-
-					<div class="fitvids-video">
-						<?php
-						$embed_code = wp_oembed_get( $video_post_url );
-
-						echo $embed_code; // phpcs:ignore WordPress.XSS.EscapeOutput.OutputNotEscaped
-						?>
-					</div>
-				<?php
-				endif;
 				?>
 
 				<div class="cm-post-content">
