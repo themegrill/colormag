9d36330cdf250ef5a93a4f96d98a8529618d6933
som3669 <somshrestha3669@gmail.com>
2024-08-04 08:57:01 +0545

    add front-page options

--- inc/customizer/options/options.php
+++ inc/customizer/options/options.php
@@ -6,3 +6,4 @@ require __DIR__ . '/global/container.php';
 require __DIR__ . '/global/sidebar.php';
 require __DIR__ . '/global/button.php';
 require __DIR__ . '/global/typography.php';
+require __DIR__ . '/front-page/front-page.php';
--- inc/customizer/panels-sections/panels-sections.php
+++ inc/customizer/panels-sections/panels-sections.php
@@ -28,35 +28,40 @@ $panel_options_id = array(
 );
 
 $section_option_id = array(
-	'colormag_global_colors_section'     => array(
+	'colormag_global_colors_section'      => array(
 		'title'    => esc_html__( 'Colors', 'colormag' ),
 		'panel'    => 'colormag_global_panel',
 		'priority' => 10,
 	),
-	'colormag_category_colors_section'   => array(
+	'colormag_category_colors_section'    => array(
 		'title'    => esc_html__( 'Category Colors', 'colormag' ),
 		'panel'    => 'colormag_global_panel',
 		'priority' => 20,
 	),
-	'colormag_global_container_section'  => array(
+	'colormag_global_container_section'   => array(
 		'title'    => esc_html__( 'Container', 'colormag' ),
 		'panel'    => 'colormag_global_panel',
 		'priority' => 30,
 	),
-	'colormag_global_sidebar_section'    => array(
+	'colormag_global_sidebar_section'     => array(
 		'title'    => esc_html__( 'Sidebar', 'colormag' ),
 		'panel'    => 'colormag_global_panel',
 		'priority' => 40,
 	),
-	'colormag_global_typography_section' => array(
+	'colormag_global_typography_section'  => array(
 		'title'    => esc_html__( 'Typography', 'colormag' ),
 		'panel'    => 'colormag_global_panel',
 		'priority' => 50,
 	),
-	'colormag_button_section'            => array(
+	'colormag_button_section'             => array(
 		'title'    => esc_html__( 'Button', 'colormag' ),
 		'panel'    => 'colormag_global_panel',
 		'priority' => 60,
+	),
+	'colormag_front_page_general_section' => array(
+		'title'    => esc_html__( 'General', 'colormag' ),
+		'panel'    => 'colormag_front_page_panel',
+		'priority' => 10,
 	),
 );
 
