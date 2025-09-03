86ca1eb29341ee04c03fb916c97dbce2026213fb
som3669 <somshrestha3669@gmail.com>
2025-09-03 18:05:23 +0545

    internal released

--- assets/build/meta.asset.php
+++ assets/build/meta.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-hooks', 'wp-i18n', 'wp-plugins'), 'version' => 'b846479f4384c61be954');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-hooks', 'wp-i18n', 'wp-plugins'), 'version' => '8b7ddb13f6831a3f0723');
--- readme.txt
+++ readme.txt
@@ -2,11 +2,11 @@
 
 Contributors: themegrill
 Author: ThemeGrill
-Version: 4.0.20
+Version: 4.1.0
 Tested up to: 6.8
 Requires PHP: 7.4
 Requires at least: 4.7
-Stable tag: 4.0.20
+Stable tag: 4.1.0
 
 License: GNU General Public License, version 3 (GPLv3)
 License URI: http://www.gnu.org/licenses/gpl-3.0.txt
