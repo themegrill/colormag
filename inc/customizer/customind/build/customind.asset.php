cbd7cd2cdc3689a467f2edb44743d8d877348b8a
Prajjwal Poudel <iamprazol@gmail.com>
2026-06-01 10:29:03 +0545

    CMAG-519 Fix - UI Breaks in Color Palette Dropdown Selections (HEX, RGB, HSL)

--- inc/customizer/customind/build/customind.asset.php
+++ inc/customizer/customind/build/customind.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-media-utils'), 'version' => 'a1e0d3036ba517a68390');
+<?php return array('dependencies' => array('jquery', 'lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-media-utils'), 'version' => '4e7ce1a54c42393f109a');
