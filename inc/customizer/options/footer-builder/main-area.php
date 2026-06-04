daa4ce708add9577c8edaf157f0a1ce1cf93f699
som3669 <somshrestha3669@gmail.com>
2026-06-04 21:21:06 +0545

    feat: add pro extensibility hooks to remaining customizer options

--- inc/customizer/options/footer-builder/main-area.php
+++ inc/customizer/options/footer-builder/main-area.php
@@ -222,3 +222,5 @@ $options = array(
 );
 
 colormag_customind()->add_controls( $options );
+
+do_action( 'colormag_customizer_footer_builder_main_area_pro_options', $wp_customize );
--- inc/customizer/options/header-builder/bottom-area.php
+++ inc/customizer/options/header-builder/bottom-area.php
@@ -125,3 +125,5 @@ $options = array(
 );
 
 colormag_customind()->add_controls( $options );
+
+do_action( 'colormag_customizer_header_builder_bottom_area_pro_options', $wp_customize );
