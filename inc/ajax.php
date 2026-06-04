d7062785d01be3a85c6dd8639739a4de45e7dfee
som3669 <somshrestha3669@gmail.com>
2026-06-04 11:05:40 +0545

    fix: defer colormag_ajax_pro_hooks to after_setup_theme so add-on listeners attach first

--- inc/ajax.php
+++ inc/ajax.php
@@ -16,6 +16,13 @@ if ( ! defined( 'ABSPATH' ) ) {
  * Allow the pro version (or any add-on) to register its AJAX handlers.
  *
  * The pro AJAX functions (load more, infinite scroll, single post
- * navigation and comments) hook into this action.
+ * navigation and comments) hook into this action. It is fired on
+ * `after_setup_theme` so that all include files (including the pro
+ * AJAX file) have been loaded and their listeners attached first.
  */
-do_action( 'colormag_ajax_pro_hooks' );
+add_action(
+	'after_setup_theme',
+	function () {
+		do_action( 'colormag_ajax_pro_hooks' );
+	}
+);
