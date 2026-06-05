b53b1d611d119caf5158dec8034a8c08c8de791c
som3669 <somshrestha3669@gmail.com>
2026-06-05 12:09:43 +0545

    sync with pro

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'themegrill/colormag',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'c339a881cb28726088285540ca94f0ee04b14ffe',
+        'type' => 'theme',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'themegrill/colormag' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'c339a881cb28726088285540ca94f0ee04b14ffe',
+            'type' => 'theme',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'themegrill/themegrill-sdk' => array(
+            'pretty_version' => '1.0.1',
+            'version' => '1.0.1.0',
+            'reference' => 'bdcd3b56256ce3126054d0dabb22fe8563b0becc',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../themegrill/themegrill-sdk',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
