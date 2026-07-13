4923fcd906184693b927961c395ebd59da3216c8
Prajjwal Poudel <iamprazol@gmail.com>
2026-07-13 08:47:15 +0545

    Version bump to v4.2.1 for internal release

--- assets/build/dashboard.asset.php
+++ assets/build/dashboard.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '7da7b341ec47ed95c7f2');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '5c899db0866e52e0920a');
--- assets/build/meta.asset.php
+++ assets/build/meta.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-hooks', 'wp-i18n', 'wp-plugins'), 'version' => '89382085d21583a7022e');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-hooks', 'wp-i18n', 'wp-plugins'), 'version' => '36d614f10571a691d3c9');
